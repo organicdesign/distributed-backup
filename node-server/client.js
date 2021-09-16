dde70b069182371df6ed8dd3c858aa81a1d821de
Saul <saul@organicdesign.co.nz>
2021-09-16 16:22:11 +1200

    Refactor client.

--- node-server/client.js
+++ node-server/client.js
@@ -7,33 +7,33 @@ const client = new net.Socket();
 let sent = null;
 let position = 0;
 
+const sendData = () => {
+	const stdout = execSync(`../read ./package.json 100 ${position}`);
+
+	if (stdout.length === 0)
+		client.destroy();
+
+	client.write(stdout);
+
+	const shasum = crypto.createHash("sha1");
+	shasum.update(stdout.toString());
+	sent = shasum.digest("hex");
+};
+
 client.connect(8080, "127.0.0.1", () => {
 	console.log("Connected");
 
-	const sendData = () => {
-		const stdout = execSync(`../read ./package.json 100 ${position}`);
-
-		if (stdout.length === 0)
-			client.destroy();
-
-		client.write(stdout);
-
-		const shasum = crypto.createHash("sha1");
-		shasum.update(stdout.toString());
-		sent = shasum.digest("hex");
-	};
-
 	sendData();
+});
 
-	client.on("data", data => {
-		if (sent === data.toString()) {
-			console.log("Got correct ack!");
-			position++;
-			sendData();
-		} else {
-			console.log("Wrong ack!");
-		}
-	});
+client.on("data", data => {
+	if (sent === data.toString()) {
+		console.log("Got correct ack!");
+		position++;
+		sendData();
+	} else {
+		console.log("Wrong ack!");
+	}
 });
 
 client.on("close", () => {
