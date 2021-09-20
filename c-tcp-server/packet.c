bd2f90cb328957e7b192922f7fea57ba62bccd7a
Saul <saul@organicdesign.co.nz>
2021-09-20 13:52:40 +1200

    Add functions for calculating packet size.

--- c-tcp-server/packet.c
+++ c-tcp-server/packet.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #ifndef PACKET_C
 #define PACKET_C
@@ -51,17 +52,59 @@ struct Packet* createQueryPacket (unsigned int key) {
 	return p;
 }
 
+int getPacketSize (struct Packet* packet) {
+	switch(packet->type) {
+		case KEY:
+			return
+				sizeof(packet->type) +
+				sizeof(packet->key) +
+				strlen(packet->data);
+
+		case DATA:
+			return
+				sizeof(packet->type) +
+				sizeof(packet->key) +
+				sizeof(packet->position) +
+				strlen(packet->data);
+
+		case QUERY:
+			return
+				sizeof(packet->type) +
+				sizeof(packet->key);
+
+		default:
+			printf("Packet did not match any known types. Type: #%i", packet->type);
+			return -1;
+	}
+}
+
+int convertPacketToBuffer (unsigned char* buffer, struct Packet* packet, unsigned int bufferSize) {
+	if (bufferSize < getPacketSize(packet)) {
+		printf(
+			"Buffer is too small:\n"
+			"  Packet size: %i\n"
+			"  Buffer size: %i\n",
+			getPacketSize(packet),
+			bufferSize
+		);
+
+		return -1;
+	}
+}
+
 void printPacket (struct Packet* packet) {
 	printf(
 		"Packet:\n"
 		"  Type:\t\t%s\n"
 		"  Key:\t\t%u\n"
 		"  Position:\t%li\n"
-		"  Data:\t\t%s\n",
+		"  Data:\t\t%s\n"
+		"  Size:\t\t%i\n",
 		TransportTypeNames[packet->type],
 		packet->key,
 		packet->position,
-		packet->data
+		packet->data,
+		getPacketSize(packet)
 	);
 }
 
