09db8e7a612f64385929dc4f00c3a431f0b69d4f
Saul <saul@organicdesign.co.nz>
2021-09-27 11:26:01 +1300

    Simplify multiplexing.

--- node-scheduler/retriever.js
+++ node-scheduler/retriever.js
@@ -1,26 +1,32 @@
-const maxSlots = 60;
 const maxBandwidth = 0.5; // 50% bandwidth use by this scheduler.
 
 const data = require("./retriever-data.json")
-	.filter(file => file.priority * maxSlots >= 1)
+	.filter(file => file.priority >= 0.01)
+	.map(file => {
+		file.priority *= maxBandwidth;
+		return file;
+	})
 	.sort((a,  b) => b.priority - a.priority);
 
-let sum = 0, err = 0;
-for (const server of data) {
-	const exactCount = server.priority * maxSlots * maxBandwidth + err;
-	const count = Math.round(exactCount);
-
-	sum += count;
-	err = exactCount - count;
-
-	server.slots = count;
-}
-
-let counter = 0;
- setInterval(() => {
+let counter = 0, serverCounter = 0, currentServer = 0;
+setInterval(() => {
 	if (counter % Math.round(1 / maxBandwidth) == 0) {
-		console.log("Do Something.");
+		console.log(data[currentServer].server);
+
+		// Increment serverCounter every time we do a server call.
+		serverCounter++;
+
+		// Change server we are doing work for.
+		if (serverCounter >= data[currentServer].priority * 100) {
+			currentServer = (currentServer + 1) % data.length;
+			serverCounter = 0;
+		}
 	} else
 		console.log("Do nothing.");
-	counter++;
+
+	// Increment counter
+	counter = (counter + 1) % 100;
+
+	if (counter == 0)
+		console.log("_____________________________________")
 }, 1000);
