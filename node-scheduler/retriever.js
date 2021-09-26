312ada6d37d3bc9c86406cbffce90d62e838dab9
Saul <saul@organicdesign.co.nz>
2021-09-27 12:41:37 +1300

    Create a function for calculating optimal work/sleep.

--- node-scheduler/retriever.js
+++ node-scheduler/retriever.js
@@ -1,5 +1,6 @@
-const maxBandwidth = 0.5; // 50% bandwidth use by this scheduler.
+const maxBandwidth = 0.12; // 50% bandwidth use by this scheduler.
 
+// Obtain and sort the data.
 const data = require("./retriever-data.json")
 	.filter(file => file.priority >= 0.01)
 	.map(file => {
@@ -8,9 +9,76 @@ const data = require("./retriever-data.json")
 	})
 	.sort((a,  b) => b.priority - a.priority);
 
+const calculateIntervals = () => {
+	// We need a greatest common denominator function for the next calculations.
+	const gcd = (a, b) => {
+		a = Math.abs(a);
+		b = Math.abs(b);
+
+		if (b > a)
+			[a, b] = [b, a];
+
+		while (true) {
+			if (b == 0)
+				return a;
+
+			a %= b;
+
+			if (a == 0)
+				return b;
+
+			b %= a;
+		}
+	};
+
+	// Convert the bandwidth into slots out of 100
+	const bandwidthAsSlots = maxBandwidth * 100;
+
+	// Work out what number we need to divide by to reduce the slots value.
+	const gcdWork = gcd(bandwidthAsSlots, 100 - bandwidthAsSlots);
+
+	// Calculate the number of slots of work and sleep.
+	const slotsOfWork = bandwidthAsSlots / gcdWork;
+	const slotsOfSleep = (100 - bandwidthAsSlots) / gcdWork;
+
+	// Predefine the return values so if left unchanged they have the default.
+	let work = 1, sleep = 1, rWork = 0, rSleep = 0;
+
+	// We need to check if we will have a remainder of work or sleep.
+	if (slotsOfWork > slotsOfSleep) {
+		// Calculate the remainder of work.
+		rWork = slotsOfWork % slotsOfSleep;
+
+		// Use the remainder to reduce the work value as low as possible.
+		const rGcdWork = gcd(slotsOfWork - rWork, slotsOfSleep);
+
+		// We only need to work out how much work needs to be done for 1 sleep.
+		work = (slotsOfWork - rWork) / rGcdWork;
+	} else {
+		// Calculate the remainder of sleep.
+		rSleep = slotsOfSleep % slotsOfWork;
+
+		// Use the remainder to reduce the sleep value as low as possible.
+		const rGcdWork = gcd(slotsOfWork, slotsOfSleep - rSleep);
+
+		// We only need to work out how much sleep needs to be done for 1 work.
+		sleep = (slotsOfSleep - rSleep) / rGcdWork;
+	}
+
+	return {
+		work,
+		sleep,
+		rWork,
+		rSleep
+	};
+};
+
+console.log(calculateIntervals());
+
+// Set interval.
 let counter = 0, serverCounter = 0, currentServer = 0;
 setInterval(() => {
-	if (counter % Math.round(1 / maxBandwidth) == 0) {
+	if (counter < slotsOfWork) {
 		console.log(data[currentServer].server);
 
 		// Increment serverCounter every time we do a server call.
@@ -25,7 +93,7 @@ setInterval(() => {
 		console.log("Do nothing.");
 
 	// Increment counter
-	counter = (counter + 1) % 100;
+	counter = (counter + 1) % (slotsOfWork + slotsOfSleep);
 
 	if (counter == 0)
 		console.log("_____________________________________")
