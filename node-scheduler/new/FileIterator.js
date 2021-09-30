b7e5bcdfd3a12579f7c2e172892745ce8327e4e9
Saul <saul@organicdesign.co.nz>
2021-10-01 11:18:14 +1300

    Document the file iterator.

--- node-scheduler/new/FileIterator.js
+++ node-scheduler/new/FileIterator.js
@@ -3,27 +3,51 @@ const fs = require("fs");
 const { CID } = require("multiformats/cid");
 
 class FileIterator {
+	/**
+	 * Create a new file iterator.
+	 *
+	 * @param {string} path The path to the file to iterate over.
+	 */
 	constructor (path) {
 		this._hashes = null;
 		this._path = path;
 	}
 
+	/**
+	 * Log the internal hashes to the console.
+	 */
+	debug () {
+		console.log(this._hashes)
+	}
+
+	/**
+	 * Read and convert the file into chunks.
+	 *
+	 * @return {Promise<Set.CID>} The set of CID's pointing to the chunks.
+	 */
 	async chunkify () {
-		if (this.hashes)
+		if (this._hashes)
 			return;
 
 		const ipfs = await ipfsAsync;
 		const content = fs.readFileSync(this._path);
 		this._root = (await ipfs.add({content})).cid;
-		this._hashes = await this.getAllLinks(this._root);
+		this._hashes = await this._getAllLinks(this._root);
 		this._iterator = this._hashes.values();
+
+		return this._hashes;
 	}
 
-	debug () {
-		console.log(this._hashes)
-	}
-
-	async getAllLinks (hash, linkSet = null) {
+	/**
+	 * Get a set of all links and sub-links of a CID.
+	 *
+	 * @param {CID} hash The root CID to get links for.
+	 * @param {Set.CID} linkSet The set of CIDs already found. Defaults to an
+	 * empty set.
+	 *
+	 * @return {Set.CID} The set of all links found in the CID.
+	 */
+	async _getAllLinks (hash, linkSet = null) {
 		if (!linkSet)
 			linkSet = new Set();
 
@@ -39,10 +63,17 @@ class FileIterator {
 		return linkSet;
 	}
 
+	/**
+	 * Get the next chunk of this file. The method 'chunkify' must be called
+	 * first.
+	 *
+	 * @return {CID} The CID of the next chunk.
+	 */
 	next () {
 		return this._iterator.next();
 	}
 
+	// Define the iterator method.
 	*[Symbol.iterator] () {
 		for (;;) {
 			const next = this.next();
