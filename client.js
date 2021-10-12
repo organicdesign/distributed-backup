e3b14e240e345f76664810d6651b556b3cb0a322
Saul <saul@organicdesign.co.nz>
2021-10-12 13:42:07 +1300

    Create proof test program.

--- client.js
+++ client.js
@@ -1,5 +1,8 @@
 const net = require("net");
+const fs = require("fs");
+const { execSync } = require("child_process");
 
+const ipfsAsync = require("./classes/ipfs");
 const MultiplexingClient = require("./classes/MultiplexingClient");
 
 const multiplexingClient = new MultiplexingClient("unixSocket");
@@ -9,8 +12,14 @@ const server = "localhost";
 
 const client = net.connect(port, server);
 
-client.on("data", data => {
-	console.log(data.toString());
+client.on("data", async data => {
+	//const ipfs = await ipfsAsync;
+
+	//ipfs.dag.put(data, { format: "dag-pb" });
+	fs.writeFileSync("./out", data);
+	//const stdout = execSync("cat ./out | ipfs dag put -f protobuf --input-enc protobuf");
+	//console.log(stdout);
+
 });
 
 (async () => {
