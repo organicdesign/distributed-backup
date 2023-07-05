bc467495c5f70ccde56bcb70fc17cbe95957141d
saul <saul@organicdesign.co.nz>
2023-07-05 15:04:25 +1200

    Add crypto option to file import.

--- src/fs-importer/import-file.ts
+++ src/fs-importer/import-file.ts
@@ -1,4 +1,5 @@
 import fs from "fs";
+import crypto from "crypto";
 import { CID } from "multiformats/cid";
 import { sha256 } from "multiformats/hashes/sha2";
 import { fixedSize } from "ipfs-unixfs-importer/chunker";
@@ -6,24 +7,48 @@ import * as raw from "multiformats/codecs/raw";
 import * as dagPb from "@ipld/dag-pb";
 import { PBLink, prepare, encode } from "@ipld/dag-pb";
 import { UnixFS } from "ipfs-unixfs";
-import type { ImportResult, ImporterOptions } from "./interfaces.js";
+import type { Chunker } from "ipfs-unixfs-importer/chunker";
+import type { ImportResult } from "./interfaces.js";
 import type { Filestore } from "../filestore/index.js";
 
-export const importFileNocopy = async (store: Filestore, path: string, options: ImporterOptions): Promise<ImportResult> => {
+export const importFileNocopy = async (
+	store: Filestore,
+	path: string,
+	options: { chunker?: Chunker, key?: Uint8Array } = {}
+): Promise<ImportResult> => {
 	const stream = fs.createReadStream(path, { highWaterMark: 16 * 1024 });
 	const chunker = options.chunker ?? fixedSize();
 	const links: PBLink[] = [];
 	const blockSizes: bigint[] = [];
 
 	let offset = 0n;
+	let iv: Uint8Array | undefined;
+	let cipher: crypto.Cipher | null = null;
+
+	// If key is set, setup cipher
+	if (options.key != null) {
+		iv = crypto.randomBytes(16);
+		cipher = crypto.createCipheriv("aes-256-cbc", options.key, iv, { encoding: "binary" });
+	}
 
 	// Chunk the file
-	for await (const chunk of chunker(stream)) {
+	for await (let chunk of chunker(stream)) {
+		if (cipher != null) {
+			chunk = cipher.update(chunk);
+		}
+
 		const size = BigInt(chunk.length);
 		const multihash = await sha256.digest(chunk);
 		const cid = CID.createV1(raw.code, multihash);
 
-		await store.putLink(cid, path, offset, size);
+
+		if (cipher == null) {
+			await store.putLink(cid, path, offset, size);
+		} else {
+			await store.putLink(cid, path, offset, size, iv);
+
+			iv = chunk.subarray(chunk.length - 16, chunk.length);
+		}
 
 		links.push({ Hash: cid, Tsize: chunk.length });
 		blockSizes.push(size);
