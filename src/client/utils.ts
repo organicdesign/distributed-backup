17a76c658bbe3c8a0a64448d364ad9cf671d88cc
saul <saul@organicdesign.co.nz>
2023-07-11 12:25:18 +1200

    Cleanup utils and add create middleware method.

--- src/client/utils.ts
+++ src/client/utils.ts
@@ -1,23 +1,29 @@
-import { ArgumentsCamelCase, Options, InferredOptionTypes } from "yargs";
+import { ArgumentsCamelCase, Options, InferredOptionTypes, MiddlewareFunction } from "yargs";
 
 export type Builder = Record<string, Options>
-export type Handler<T extends Builder> = (argv: ArgumentsCamelCase<InferredOptionTypes<T>>) => void | Promise<void>
+export type ToArgs<T extends Builder> = ArgumentsCamelCase<InferredOptionTypes<T>>
+export type Handler<T extends Builder> = (argv: ToArgs<T>) => void | Promise<void>
 
-export const createRawBuilder = <T extends Record<string, Options>>(options: T): T => options;
+export const createRawBuilder = <T extends Builder>(options: T): T => options;
 
 const globalOptions = createRawBuilder({
 	socket: {
 		alias: "s",
 		type: "string",
-		describe: "The path to the daemon socket."
+		describe: "The path to the daemon socket.",
+		default: "/tmp/server.socket"
 	}
 });
 
-export const createBuilder = <T extends Record<string, Options>>(options: T): T & typeof globalOptions => ({
+export const createBuilder = <T extends Builder>(options: T): T & typeof globalOptions => ({
 	...globalOptions,
 	...options
 });
 
-export const createHandler = <T extends Record<string, Options>>(
-	handler: (argv: ArgumentsCamelCase<InferredOptionTypes<T>>) => void | Promise<void>
-) => handler;
+export const createHandler = <T extends Builder>(
+	handler: Handler<T & typeof globalOptions>
+): Handler<T & typeof globalOptions> => handler;
+
+export const createMiddleware = <T extends Builder>(
+	handler: MiddlewareFunction<ToArgs<T & typeof globalOptions>>
+): MiddlewareFunction => handler as MiddlewareFunction;
