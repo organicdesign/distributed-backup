1e54540ecda84784b93103214d6a65c55ae9c771
saul <saul@organicdesign.co.nz>
2023-07-11 14:27:53 +1200

    Cleanup client.

--- src/client.ts
+++ src/client.ts
@@ -9,35 +9,10 @@ await yargs(hideBin(process.argv))
 	.demandCommand()
 	.middleware(createMiddleware(argv => {
 		argv.client = createNetClient(argv.socket);
+
+		process.on("SIGINT", () => {
+			argv.client?.close();
+			process.exit(1);
+		});
 	}))
 	.parse();
-/*
-process.on("SIGINT", () => {
-	close();
-	process.exit();
-});
-
-const path = Path.join(srcPath, "src");
-
-console.log("adding:");
-let start = performance.now();
-const cid = await rpc.request("add", { path });
-console.log("added", performance.now() - start, cid);
-
-console.log("adding:");
-start = performance.now();
-const cid2 = await rpc.request("add", { path, hashonly: true });
-console.log("added", performance.now() - start, cid2);
-
-const query = await rpc.request("query", {});
-
-console.log("query", query);
-
-const id = await rpc.request("id", {});
-
-console.log("id", id);
-
-const address = await rpc.request("address", {});
-
-console.log("address", address);
-*/
