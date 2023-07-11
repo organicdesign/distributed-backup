795e1a7470f9c7c12d07587cde30504c654c2eec
saul <saul@organicdesign.co.nz>
2023-07-11 15:36:45 +1200

    Add connect command.

---
+++ src/client/commands/connect.ts
@@ -0,0 +1,24 @@
+import { createBuilder, createHandler } from "../utils.js";
+
+export const command = "connect [address]";
+
+export const desc = "Connect to a database.";
+
+export const builder = createBuilder({
+	address: {
+		type: "string",
+		required: true
+	}
+});
+
+export const handler = createHandler<typeof builder>(async argv => {
+	if (argv.client == null) {
+		throw new Error("Failed to connect to daemon.");
+	}
+
+	const connect = await argv.client.rpc.request("connect", { address: argv.address });
+
+	console.log(connect);
+
+	argv.client.close();
+});
--- src/client/commands/index.ts
+++ src/client/commands/index.ts
@@ -2,9 +2,10 @@ import * as add from "./add.js";
 import * as query from "./query.js";
 import * as id from "./id.js";
 import * as address from "./address.js";
+import * as connect from "./connect.js";
 import type { Options } from "yargs";
 
-export default [add, query, id, address] as unknown as {
+export default [add, query, id, address, connect] as unknown as {
 	desc: string,
 	command: string,
 	builder: Record<string, Options>,
