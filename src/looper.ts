3dfd0089dfc3c69c02044f4a676e51edee9aa470
saul <saul@organicdesign.co.nz>
2023-07-20 12:42:57 +1200

    Add looper class.

---
+++ src/looper.ts
@@ -0,0 +1,34 @@
+export interface LooperOptions {
+	sleep: number
+}
+
+export class Looper {
+	private readonly options: LooperOptions;
+	private readonly method: () => void | Promise<void>;
+
+	constructor (method: () => void | Promise<void>, options: Partial<LooperOptions> = {}) {
+		this.options = {
+			sleep: options.sleep ?? 60000
+		};
+
+		this.method = method;
+	}
+
+	async run (options: { signal?: AbortSignal }) {
+		for (;;) {
+			if (options.signal?.aborted) {
+				return;
+			}
+
+			await this.method();
+			await new Promise<void>((resolve, reject) => {
+				options.signal?.addEventListener("abort", reject, { once: true });
+
+				setTimeout(() => {
+					options.signal?.removeEventListener("abort", reject);
+					resolve();
+				}, this.options.sleep)
+			});
+		}
+	}
+}
