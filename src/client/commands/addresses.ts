24bcda065a3e5d5a8483515352f11d0f32b429e6
saul <saul@organicdesign.co.nz>
2023-07-20 13:23:25 +1200

    Remove welo option from addresses.

--- src/client/commands/addresses.ts
+++ src/client/commands/addresses.ts
@@ -2,22 +2,16 @@ import { createBuilder, createHandler } from "../utils.js";
 
 export const command = "addresses";
 
-export const desc = "Get the address of the database.";
+export const desc = "Get the address of the peer.";
 
-export const builder = createBuilder({
-	type: {
-		type: "string",
-		default: "libp2p",
-		choices: ["libp2p", "welo"] as const
-	}
-});
+export const builder = createBuilder({});
 
 export const handler = createHandler<typeof builder>(async argv => {
 	if (argv.client == null) {
 		throw new Error("Failed to connect to daemon.");
 	}
 
-	const addresses = await argv.client.rpc.request("addresses", { type: argv.type });
+	const addresses = await argv.client.rpc.request("addresses", {});
 
 	console.log(addresses);
 
