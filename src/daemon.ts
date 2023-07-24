2bd7782d42331307fed428a23026cfcd9423fda5
saul <saul@organicdesign.co.nz>
2023-07-24 13:14:36 +1200

    Cleanup the daemon file.

--- src/daemon.ts
+++ src/daemon.ts
@@ -4,11 +4,11 @@ import { MemoryDatastore } from "datastore-core";
 import { MemoryBlockstore } from "blockstore-core";
 import yargs from "yargs/yargs";
 import { hideBin } from "yargs/helpers";
+import { createWelo, pubsubReplicator, bootstrapReplicator } from "../../welo/dist/src/index.js";
 import createLibp2p from "./libp2p.js";
 import { Filestore } from "./filestore/index.js";
 import { getConfig } from "./config.js";
 import * as logger from "./logger.js";
-import { createWelo, pubsubReplicator, bootstrapReplicator } from "../../welo/dist/src/index.js";
 import { Looper } from "./looper.js";
 import mainLoop from "./main-loop.js";
 import commands from "./rpc/index.js";
@@ -27,28 +27,41 @@ const argv = await yargs(hideBin(process.argv))
 	})
 	.parse();
 
-logger.lifecycle("starting");
+logger.lifecycle("starting...");
 
-const config = await getConfig();
-logger.lifecycle("loaded config");
-
+// Setup datastores and blockstores.
 const datastore = new MemoryDatastore();
 const stores = new Datastores(datastore);
 const blockstore = new Filestore(new MemoryBlockstore(), stores.get("helia/filestore"));
+
+// Setup all the modules.
+const config = await getConfig();
+logger.lifecycle("loaded config");
+
 const libp2p = await createLibp2p();
+logger.lifecycle("loaded libp2p");
+
 const helia = await createHelia({ libp2p, blockstore, datastore: stores.get("helia/datastore") });
-const cipher = await createCipher({ libp2p, datastore: stores.get("cipher"), passphrase: "super-secret" });
+logger.lifecycle("loaded helia");
 
-logger.lifecycle("started helia");
+const welo = await createWelo({
+	ipfs: helia,
+	replicators: [bootstrapReplicator(), pubsubReplicator()]
+});
+logger.lifecycle("loaded welo");
 
-const welo = await createWelo({ ipfs: helia, replicators: [bootstrapReplicator(), pubsubReplicator()] });
+const cipher = await createCipher({
+	libp2p,
+	datastore: stores.get("cipher"),
+	passphrase: "super-secret"
+});
+logger.lifecycle("loaded cipher");
 
 const groups = await createGroups({ datastore: stores.get("groups"), welo });
-
-logger.lifecycle("started welo");
+logger.lifecycle("loaded groups");
 
 const { rpc, close } = await createNetServer(argv.socket);
-logger.lifecycle("started server");
+logger.lifecycle("loaded server");
 
 const components: Components = { libp2p, cipher, helia, welo, blockstore, groups, config, stores };
 
@@ -57,14 +70,28 @@ for (const command of commands) {
 	rpc.addMethod(command.name, command.method(components))
 }
 
+// Cleanup on signal interupt.
 process.on("SIGINT", async () => {
-	logger.lifecycle("shutting down");
+	logger.lifecycle("cleaning up...");
 	await close();
+	logger.lifecycle("stopped server");
+	await groups.stop();
+	logger.lifecycle("stopped groups");
+	await cipher.stop();
+	logger.lifecycle("stopped cipher");
+	await welo.stop();
+	logger.lifecycle("stopped welo");
+	await helia.stop();
+	logger.lifecycle("stopped helia");
+	await libp2p.stop();
+	logger.lifecycle("stopped libp2p");
 	process.exit();
 });
 
+// Create the looper.
 const looper = new Looper(() => mainLoop(components), { sleep: config.tickInterval * 1000 });
 
-logger.lifecycle("ready");
+logger.lifecycle("started");
 
+// Run the main loop.
 await looper.run();
