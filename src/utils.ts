caa9e6aebc9ee5d9226847783b86ba0d22483dda
saul <saul@organicdesign.co.nz>
2023-07-26 10:12:28 +1200

    Add safe pinning utils.

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,18 @@
 import Path from "path";
 import { fileURLToPath } from "url";
+import type { Helia } from "@helia/interface";
+import type { CID } from "multiformats/cid";
 
 export const srcPath = Path.join(Path.dirname(fileURLToPath(import.meta.url)), "..");
+
+export const safePin = async (helia: Helia, cid: CID) => {
+	if (!await helia.pins.isPinned(cid)) {
+		await helia.pins.add(cid);
+	}
+};
+
+export const safeUnpin = async (helia: Helia, cid: CID) => {
+	if (await helia.pins.isPinned(cid)) {
+		await helia.pins.rm(cid);
+	}
+};
