d482556c4eb061cde90accd19e03b637fe712423
saul <saul@organicdesign.co.nz>
2023-07-26 15:30:36 +1200

    Add encode/decode to utils.

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import Path from "path";
+import { toString as uint8ArrayToString, fromString as uint8ArrayFromString } from "uint8arrays";
 import { fileURLToPath } from "url";
 import type { Helia } from "@helia/interface";
 import type { CID } from "multiformats/cid";
@@ -15,4 +16,12 @@ export const safeUnpin = async (helia: Helia, cid: CID) => {
 	if (await helia.pins.isPinned(cid)) {
 		await helia.pins.rm(cid);
 	}
+};
+
+export const encodeAny = <T = unknown>(data: T): Uint8Array => {
+	return uint8ArrayFromString(JSON.stringify(data));
+};
+
+export const decodeAny = <T = unknown>(data: Uint8Array): T => {
+	return JSON.parse(uint8ArrayToString(data));
 };
