567f3bd629e59209a06822d5067f33dcd2873513
saul <saul@organicdesign.co.nz>
2023-08-07 16:04:57 +1200

    Update query to use reference DB.

--- src/rpc/commands/query-group.ts
+++ src/rpc/commands/query-group.ts
@@ -1,11 +1,14 @@
-import all from "it-all";
-import { CID } from "multiformats/cid";
+import { Reference } from "../../database/index.js";
 import type { Components } from "../../interface.js";
 
 export const name = "query-group";
 
-export const method = ({ references }: Components) => async (params: { group: string }) => {
-	const data = await all(references.allByGroup(CID.parse(params.group)));
+export const method = ({}: Components) => async (params: { group: string }) => {
+	const data = await Reference.findAll({
+		where: {
+			group: params.group
+		}
+	});
 
 	return data;
 };
