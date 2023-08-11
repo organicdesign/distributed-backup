8526ef385964d594e9764b24e1c70a6d6e87d0f5
saul <saul@organicdesign.co.nz>
2023-08-11 15:03:38 +1200

    Add multi-downloader implementation.

---
+++ src/downloader/multi.ts
@@ -0,0 +1,132 @@
+import * as cborg from 'cborg'
+import { base36 } from "multiformats/bases/base36";
+import { Key } from "interface-datastore";
+import { equals as uint8ArrayEquals } from "uint8arrays";
+import * as dagWalkers from "../../node_modules/helia/dist/src/utils/dag-walkers.js";
+import { CustomProgressEvent } from "progress-events";
+import type { CID, Version } from "multiformats";
+import type { Helia } from "@helia/interface";
+import type { AddOptions } from "@helia/interface/pins";
+
+const DATASTORE_PIN_PREFIX = "/pin/";
+const DATASTORE_BLOCK_PREFIX = "/pinned-block/";
+const DATASTORE_ENCODING = base36;
+
+interface DatastorePinnedBlock {
+	pinCount: number
+	pinnedBy: Uint8Array[]
+}
+
+const toDSKey = (cid: CID): Key => {
+	if (cid.version === 0) {
+		cid = cid.toV1();
+	}
+
+	return new Key(`${DATASTORE_PIN_PREFIX}${cid.toString(DATASTORE_ENCODING)}`);
+}
+
+// Pin the block through helia
+const pinBlock = async (helia: Helia, cid: CID, options: AddOptions) => {
+	const blockKey = new Key(`${DATASTORE_BLOCK_PREFIX}${DATASTORE_ENCODING.encode(cid.multihash.bytes)}`)
+
+	let pinnedBlock: DatastorePinnedBlock = {
+		pinCount: 0,
+		pinnedBy: []
+	}
+
+	try {
+		pinnedBlock = cborg.decode(await helia.datastore.get(blockKey, options))
+	} catch (err: any) {
+		if (err.code !== 'ERR_NOT_FOUND') {
+			throw err
+		}
+	}
+
+	if (pinnedBlock.pinnedBy.find(c => uint8ArrayEquals(c, cid.bytes)) == null) {
+		pinnedBlock.pinCount++
+		pinnedBlock.pinnedBy.push(cid.bytes)
+	}
+
+	if (pinnedBlock.pinCount === 0) {
+		if (await helia.datastore.has(blockKey)) {
+			await helia.datastore.delete(blockKey)
+			return
+		}
+	}
+
+	await helia.datastore.put(blockKey, cborg.encode(pinnedBlock), options)
+	options.onProgress?.(new CustomProgressEvent<CID>('helia:pin:add', { detail: cid }))
+};
+
+export const add = async function * (helia: Helia, cid: CID<unknown, number, number, Version>, count: number, options: AddOptions = {}): AsyncGenerator<{ block: Uint8Array, cid: CID }[]> {
+	const pinKey = toDSKey(cid)
+
+	if (await helia.datastore.has(pinKey)) {
+		throw new Error('Already pinned');
+	}
+
+	const depth = Math.round(options.depth ?? Infinity);
+
+	if (depth < 0) {
+		throw new Error('Depth must be greater than or equal to 0');
+	}
+
+	const queue = [cid];
+
+	// Pull a block from the queue enqueing others.
+	const pullFromQueue = async (): Promise<{ block: Uint8Array, cid: CID }> => {
+		const cid = queue.shift();
+
+		if (cid == null) {
+			throw new Error("queue is empty");
+		}
+
+		const dagWalker = Object.values(dagWalkers).find(dw => dw.codec === cid.code);
+
+		if (dagWalker == null) {
+			throw new Error(`No dag walker found for cid codec ${cid.code}`);
+		}
+
+		const block = await helia.blockstore.get(cid, options);
+
+		for await (const cid of dagWalker.walk(block)) {
+			queue.push(cid);
+		}
+
+		return { cid, block };
+	};
+
+	// Pull multiple blocks from the queue in one go.
+	const pullManyFromQueue = async (count: number): Promise<{ block: Uint8Array, cid: CID }[]> => {
+		const promises: Promise<{ block: Uint8Array, cid: CID }>[] = [];
+		const results: { block: Uint8Array, cid: CID }[] = [];
+
+		for (let i = 0; i < count; i++) {
+			if (queue.length == 0) {
+				const promise = promises.shift();
+
+				if (promise == null) {
+					break;
+				}
+
+				results.push(await promise);
+				i--;
+				continue;
+			}
+
+			promises.push(pullFromQueue());
+		}
+
+		return [...results, ...(await Promise.all(promises))];
+	};
+
+	while (queue.length != 0) {
+		const blocks = await pullManyFromQueue(count);
+
+		for (const { cid } of blocks) {
+			await pinBlock(helia, cid, options);
+		}
+
+		yield blocks;
+	}
+}
