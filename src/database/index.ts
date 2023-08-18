6080023de08c543b844174a8819fe4a801950c0e
saul <saul@organicdesign.co.nz>
2023-08-18 13:22:28 +1200

    Add blocks database class.

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,4 +1,5 @@
 export { sequelize } from "./sequelize.js";
 export { Uploads } from "./uploads.js";
 export { References } from "./references.js";
+export { Blocks } from "./blocks.js";
 export { Pins } from "./pins.js";
