92bac9ee9c1f0d37bdd7c2a77818048f77ae523b
saul <saul@organicdesign.co.nz>
2023-08-18 13:26:53 +1200

    Add downloads database class.

---
+++ src/database/downloads.ts
@@ -0,0 +1,60 @@
+import { DataTypes, Model, InferAttributes, InferCreationAttributes } from "sequelize";
+import { CID } from "multiformats/cid";
+import { sequelize } from "./sequelize.js";
+
+/*
++------------------------------------------------------------------------------+
+|                                   Downloads                                  |
++----------------------------------------------------+-------------------------+
+|                     Primary Key                    |                         |
++-----------------------+-----------------------------+------------------------+
+|          CID          |          Pinned By         |           Depth         |
++-----------------------+---------------------------+--------------------------+
+*/
+class DownloadsClass extends Model<InferAttributes<DownloadsClass, { omit: "cid" | "pinnedBy" }> & { cid: string, pinnedBy: string }, InferCreationAttributes<DownloadsClass>> {
+	declare cid: CID
+	declare pinnedBy: CID
+	declare depth: number
+}
+
+export const Downloads = sequelize.define<DownloadsClass>(
+	"downloads",
+	{
+		cid: {
+			type: DataTypes.STRING(undefined, true),
+			allowNull: false,
+			primaryKey: true,
+
+			get () {
+				const str = this.getDataValue("cid");
+
+				return CID.parse(str);
+			},
+
+			set (value: CID) {
+				this.setDataValue("cid", value.toString());
+			}
+		},
+
+		pinnedBy: {
+			type: DataTypes.STRING(undefined, true),
+			allowNull: false,
+			primaryKey: true,
+
+			get () {
+				const str = this.getDataValue("pinnedBy");
+
+				return CID.parse(str);
+			},
+
+			set (value: CID) {
+				this.setDataValue("pinnedBy", value.toString());
+			}
+		},
+
+		depth: {
+			type: DataTypes.BIGINT(),
+			allowNull: false
+		}
+	}
+);
--- src/database/index.ts
+++ src/database/index.ts
@@ -2,4 +2,5 @@ export { sequelize } from "./sequelize.js";
 export { Uploads } from "./uploads.js";
 export { References } from "./references.js";
 export { Blocks } from "./blocks.js";
+export { Downloads } from "./downloads.js";
 export { Pins } from "./pins.js";
