3daa57e6972e9dcf11a73a651df69273af0d015e
saul <saul@organicdesign.co.nz>
2023-10-03 17:35:49 +1300

    Add implementation for add to group.

--- src/rpc/commands/add-to-group.ts
+++ src/rpc/commands/add-to-group.ts
@@ -1,3 +1,4 @@
+import { CID } from "multiformats/cid";
 import type { Components } from "../../interface.js";
 
 export const name = "add-to-group";
@@ -7,5 +8,17 @@ export const method = (components: Components) => async (params: { cid: string, 
 		throw new Error("Replace is not implemented yet.");
 	}
 
-	return "not implemented";
+	const upload = await components.uploads.findOne({ where: { cid: params.cid }});
+
+	if (upload == null) {
+		throw new Error(`cannot find upload: ${params.cid}`);
+	}
+
+	await components.groups.addTo(CID.parse(params.group), {
+		author: components.libp2p.peerId.toBytes(),
+		cid: CID.parse(params.cid),
+		encrypted: upload.encrypt,
+		timestamp: Date.now(),
+		links: []
+	});
 };
