035d9474be4689ae038d1b0757922ba3920cd00a
saul <saul@organicdesign.co.nz>
2023-10-04 11:10:08 +1300

    Add util for adding a pin reference to helia.

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,8 @@ import Path from "path";
 import * as cborg from "cborg";
 import { CID } from "multiformats/cid";
 import { fileURLToPath } from "url";
+import { base36 } from "multiformats/bases/base36";
+import { Datastore, Key } from "interface-datastore";
 import type { Helia } from "@helia/interface";
 
 export const srcPath = Path.join(Path.dirname(fileURLToPath(import.meta.url)), "..");
@@ -29,4 +31,26 @@ export const encodeAny = <T = unknown>(data: T): Uint8Array => {
 
 export const decodeAny = <T = unknown>(data: Uint8Array): T => {
 	return cborg.decode(data);
+};
+
+export const addPinRef = async ({ datastore }: { datastore: Datastore }, cid: CID, options?: { depth?: number }): Promise<void> => {
+	if (cid.version === 0) {
+		cid = cid.toV1();
+	}
+
+	const pinKey= new Key(`/pin/${cid.toString(base36)}`);
+
+	if (await datastore.has(pinKey)) {
+		throw new Error("Already pinned");
+	}
+
+	const depth = Math.round(options?.depth ?? Infinity)
+
+	if (depth < 0) {
+		throw new Error('Depth must be greater than or equal to 0')
+	}
+
+	const pin = { depth, metadata: {} };
+
+	await datastore.put(pinKey, cborg.encode(pin));
 };
