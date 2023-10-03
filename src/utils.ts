0771e503710ddd4a23d1ce15126d7e5e42481d8a
saul <saul@organicdesign.co.nz>
2023-10-04 11:28:21 +1300

    Add util for adding a block reference.

--- src/utils.ts
+++ src/utils.ts
@@ -4,7 +4,19 @@ import { CID } from "multiformats/cid";
 import { fileURLToPath } from "url";
 import { base36 } from "multiformats/bases/base36";
 import { Datastore, Key } from "interface-datastore";
+import { equals as uint8ArrayEquals } from "uint8arrays";
 import type { Helia } from "@helia/interface";
+
+
+interface DatastorePin {
+	depth: number
+	metadata: Record<string, string | number | boolean>
+}
+
+interface DatastorePinnedBlock {
+	pinCount: number
+	pinnedBy: Uint8Array[]
+}
 
 export const srcPath = Path.join(Path.dirname(fileURLToPath(import.meta.url)), "..");
 
@@ -50,7 +62,38 @@ export const addPinRef = async ({ datastore }: { datastore: Datastore }, cid: CI
 		throw new Error('Depth must be greater than or equal to 0')
 	}
 
-	const pin = { depth, metadata: {} };
+	const pin: DatastorePin = { depth, metadata: {} };
 
 	await datastore.put(pinKey, cborg.encode(pin));
+};
+
+export const addBlockRef = async ({ datastore }: { datastore: Datastore }, cid: CID, by: CID): Promise<void> => {
+	if (cid.version === 0) {
+		cid = cid.toV1();
+	}
+
+	if (by.version === 0) {
+		by = by.toV1();
+	}
+
+	const blockKey = new Key(`/pinned-block/${base36.encode(cid.multihash.bytes)}`);
+
+	let pinnedBlock: DatastorePinnedBlock = { pinCount: 0, pinnedBy: [] };
+
+	try {
+		pinnedBlock = cborg.decode(await datastore.get(blockKey));
+	} catch (err: any) {
+		if (err.code !== 'ERR_NOT_FOUND') {
+			throw err;
+		}
+	}
+
+	if (pinnedBlock.pinnedBy.find(c => uint8ArrayEquals(c, by.bytes)) != null) {
+		return;
+	}
+
+	pinnedBlock.pinCount++;
+	pinnedBlock.pinnedBy.push(by.bytes);
+
+	await datastore.put(blockKey, cborg.encode(pinnedBlock));
 };
