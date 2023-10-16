bf9298c19266f5c48091b89d62bd3276bae28998
saul <saul@organicdesign.co.nz>
2023-10-16 13:57:28 +1300

    Add arguments to the key generation tool.

--- src/generate-key-file.ts
+++
@@ -1,8 +0,0 @@
-import Path from "path";
-import { generateMnemonic, generateKeyFile } from "./key-manager/utils.js";
-import { projectPath } from "./utils.js";
-
-const mnemonic = generateMnemonic();
-const name = "my server";
-
-await generateKeyFile(Path.join(projectPath, "./config/key.json"), mnemonic, name);
