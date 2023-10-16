705bdf1e524eeab1d5731a016bbb8ee4dc7f17d0
saul <saul@organicdesign.co.nz>
2023-10-17 12:27:42 +1300

    Provide better interface for group listing.

--- src/client/commands/list-groups.ts
+++ src/client/commands/list-groups.ts
@@ -11,9 +11,20 @@ export const handler = createHandler<typeof builder>(async argv => {
 		throw new Error("Failed to connect to daemon.");
 	}
 
-	const groups = await argv.client.rpc.request("list-groups", {});
+	const groups: { cid: string, name: string, count: number, peers: number }[] = await argv.client.rpc.request("list-groups", {});
 
-	console.log(groups);
+	console.log(`${"Name".padEnd(10)}${"Items".padEnd(10)}${"Peers".padEnd(10)}${"CID".padEnd(62)}`);
+
+	for (const group of groups) {
+		let str = "";
+
+		str += group.name.slice(0, 8).padEnd(10);
+		str += `${group.count}`.slice(0, 8).padEnd(10);
+		str += `${group.peers}`.slice(0, 8).padEnd(10);
+		str += group.cid.padEnd(62);
+
+		console.log(str);
+	}
 
 	argv.client.close();
 });
--- src/rpc/commands/list-groups.ts
+++ src/rpc/commands/list-groups.ts
@@ -1,9 +1,46 @@
-import type { Components } from "../../interface.js";
+import * as dagCbor from "@ipld/dag-cbor";
+import { CID } from "multiformats/cid";
+import type { Components, Entry } from "../../interface.js";
 
 export const name = "list-groups";
 
 export const method = (components: Components) => async () => {
-	const groups = [...components.groups.all()].map(p => p.key);
+	const promises: Promise<{ cid: string, name: string, count: number, peers: number }>[] = [];
 
-	return groups;
+	for (const { key: cid, value: database } of components.groups.all()) {
+		promises.push((async () => {
+			const index = await database.store.latest();
+			let items = 0;
+			let peers = 0;
+
+			await Promise.all([
+				(async () => {
+					try {
+						for await (const pair of index.query({})) {
+							const entry = dagCbor.decode(pair.value) as Entry;
+
+							if (entry != null) {
+								items++;
+							}
+						}
+					} catch (error) {
+						// Do nothing
+					}
+				})(),
+				(async () => {
+					try {
+						for await (const _ of components.libp2p.services.dht.findProviders(CID.parse(cid), { signal: AbortSignal.timeout(1000) })) {
+							peers++;
+						}
+					} catch (error) {
+						// Do nothing
+					}
+				})()
+			]);
+
+			return { cid, name: database.manifest.name, count: items, peers };
+		})());
+	}
+
+	return await Promise.all(promises);
 };
