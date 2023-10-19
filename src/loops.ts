1c0087823d7689944a9a81fe2039b5625406fad2
saul <saul@organicdesign.co.nz>
2023-10-19 14:09:01 +1300

    Run downloads in parallel.

--- src/loops.ts
+++ src/loops.ts
@@ -4,50 +4,64 @@ import { diskToUploads } from "./sync/disk-to-uploads.js";
 import { uploadsToGroups } from "./sync/uploads-to-groups.js";
 import { refsToPins } from "./sync/refs-to-pins.js";
 import { linearWeightTranslation } from "./utils.js";
+import * as logger from "./logger.js";
+import { pipe } from "it-pipe";
+import { take, parallelMap, collect } from "streaming-iterables";
 import type { Components } from "./interface.js";
+import type { CID } from "multiformats/cid";
 
 export const syncLoop = async (components: Components) => {
-	// logger.tick("started");
-
-	//console.log("syncing to uploads");
+	// logger.lifecycle("started");
 	await diskToUploads(components);
-	//console.log("syncing to groups");
+	// logger.tick("syncing to groups");
 	await uploadsToGroups(components);
-	//console.log("syncing to references");
+	// logger.tick("syncing to references");
 	await groupsToRefs(components);
-	//console.log("syncing to pins");
+	// logger.tick("syncing to pins");
 	await refsToPins(components);
-	//console.log("syncing complete");
-
-	// logger.tick("finished");
+	// logger.lifecycle("finished");
 };
 
 export const downloadLoop = async (components: Components) => {
+	//logger.tick("STARTED");
 	const pins = await components.pinManager.getActiveDownloads();
+	// logger.tick("GOT ACTIVE");
 
 	const remoteContents = await components.remoteContent.findAll({
 		where: {
 			[Op.or]: pins.map(p => ({ cid: p.toString() }))
 		}
 	});
+	// logger.tick("GOT REMOTE CONTENTS");
+
+	const promises: Promise<unknown>[] = [];
 
 	for (const pin of pins) {
 		const remoteContent = remoteContents.find(r => pin.equals(r.cid));
 		const priority = remoteContent?.priority ?? 100;
-		const weight = Math.ceil(linearWeightTranslation(priority / 100) * 10); // The 10 here is the max number of slots per pin.
+		const weight = Math.ceil(linearWeightTranslation(priority / 100) * 100); // The 100 here is the max number of slots per pin.
 
-		for (let i = 0; i < weight; i++) {
-			try {
-				const { done, value: f } = await components.pinManager.downloadPin(pin).next();
+		promises.push((async () => {
+			const promises: Promise<unknown>[] = [];
 
-				if (!done) {
-					await f();
-				} else {
-					break;
+			for (let i = 0; i < weight; i++) {
+				try {
+					await pipe(
+						components.pinManager.downloadPin(pin),
+						i => take(weight, i),
+						parallelMap(Infinity, (f: () => Promise<CID>) => f()),
+						i => collect(i)
+					);
+				} catch (error) {
+					console.error(`failed to download pin: ${pin}`, error);
 				}
-			} catch (error) {
-				console.error(`failed to download pin: ${pin}, error: ${error}`);
 			}
-		}
+
+			await Promise.all(promises);
+		})());
 	}
+
+	await Promise.all(promises);
+	//logger.tick("FINISHED");
 };
+////
