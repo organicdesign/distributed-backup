07a4fd5df058392eb191bedf9fb9d3ea3e94f6aa
saul <saul@organicdesign.co.nz>
2023-10-20 16:03:35 +1300

    Use streaming iterables to simplify downloads.

--- src/loops.ts
+++ src/loops.ts
@@ -6,7 +6,8 @@ import { refsToPins } from "./sync/refs-to-pins.js";
 import { linearWeightTranslation } from "./utils.js";
 import * as logger from "./logger.js";
 import { pipe } from "it-pipe";
-import { take, parallelMap, collect } from "streaming-iterables";
+import { take, collect, tap, parallelMerge } from "streaming-iterables";
+import parallel from "it-parallel";
 import type { Components } from "./interface.js";
 import type { CID } from "multiformats/cid";
 
@@ -22,10 +23,17 @@ export const syncLoop = async (components: Components) => {
 	// logger.lifecycle("finished");
 };
 
+let size = 0n;
+let start: number | null = null;
+
 export const downloadLoop = async (components: Components) => {
 	//logger.tick("STARTED");
 	const pins = await components.pinManager.getActiveDownloads();
 	// logger.tick("GOT ACTIVE");
+
+	if (pins.length !== 0 && start == null) {
+		start = Date.now();
+	}
 
 	const remoteContents = await components.remoteContent.findAll({
 		where: {
@@ -34,34 +42,64 @@ export const downloadLoop = async (components: Components) => {
 	});
 	// logger.tick("GOT REMOTE CONTENTS");
 
-	const promises: Promise<unknown>[] = [];
+	const SLOTS = 10;
 
-	for (const pin of pins) {
-		const remoteContent = remoteContents.find(r => pin.equals(r.cid));
-		const priority = remoteContent?.priority ?? 100;
-		const weight = Math.ceil(linearWeightTranslation(priority / 100) * 100); // The 100 here is the max number of slots per pin.
+	const batchDownload = function * (itr: Iterable<CID>): Generator<AsyncIterable<() => Promise<CID>>> {
+		for (const cid of itr) {
+			const remoteContent = remoteContents.find(r => cid.equals(r.cid));
+			const priority = remoteContent?.priority ?? 100;
+			const weight = Math.ceil(linearWeightTranslation(priority / 100) * SLOTS);
 
-		promises.push((async () => {
-			const promises: Promise<unknown>[] = [];
+			const downloader = components.pinManager.downloadPin(cid);
 
-			for (let i = 0; i < weight; i++) {
-				try {
-					await pipe(
-						components.pinManager.downloadPin(pin),
-						i => take(weight, i),
-						parallelMap(Infinity, (f: () => Promise<CID>) => f()),
-						i => collect(i)
-					);
-				} catch (error) {
-					console.error(`failed to download pin: ${pin}`, error);
-				}
-			}
+			yield take(weight, downloader);
+		}
+	};
 
-			await Promise.all(promises);
-		})());
-	}
+	const logSpeed = tap<CID>(async cid => {
+		try {
+			const block = await components.blockstore.get(cid);
 
-	await Promise.all(promises);
+			size += BigInt(block.length);
+
+			const deltaTime = Date.now() - (start ?? 0);
+			const kbs = size / BigInt(deltaTime);
+
+			console.log(`download: ${kbs} kb/s`);
+		} catch (error) {
+			console.error("TAP ERROR", error);
+			throw error;
+		}
+	});
+
+	await pipe(
+		pins,
+		batchDownload,
+		i => parallelMerge<AsyncIterable<() => Promise<CID>>[]>(...i),
+		i => parallel(i, { concurrency: SLOTS, ordered: false }),
+		logSpeed,
+		i => collect(i)
+	);
+
 	//logger.tick("FINISHED");
 };
-////
+
+/*
+file://node_modules/@libp2p/kad-dht/src/query/query-path.ts:227
+      deferred.reject(new CodeError('Query aborted', 'ERR_QUERY_ABORTED'))
+                      ^
+CodeError: Query aborted
+    at EventTarget.<anonymous> (file://node_modules/@libp2p/kad-dht/src/query/query-path.ts:227:23)
+    at EventTarget.[nodejs.internal.kHybridDispatch] (node:internal/event_target:757:20)
+    at EventTarget.dispatchEvent (node:internal/event_target:692:26)
+    at abortSignal (node:internal/abort_controller:369:10)
+    at AbortController.abort (node:internal/abort_controller:403:5)
+    at EventTarget.onAbort (file://node_modules/any-signal/src/index.ts:14:16)
+    at EventTarget.[nodejs.internal.kHybridDispatch] (node:internal/event_target:757:20)
+    at EventTarget.dispatchEvent (node:internal/event_target:692:26)
+    at abortSignal (node:internal/abort_controller:369:10)
+    at AbortController.abort (node:internal/abort_controller:403:5) {
+  code: 'ERR_QUERY_ABORTED',
+  props: {}
+}
+*/
