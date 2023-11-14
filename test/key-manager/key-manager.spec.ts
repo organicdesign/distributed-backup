2d74a818e2b215a158a1980c5d3ff88b12e0bbb9
saul <saul@organicdesign.co.nz>
2023-11-14 13:44:16 +1300

    Add get psk keyManager tests.

---
+++ test/key-manager/key-manager.spec.ts
@@ -0,0 +1,26 @@
+import { describe, it, before } from "node:test";
+import assert from "assert/strict";
+import { generateKeyData, parseKeyData } from "../../src/key-manager/utils.js";
+import { KeyManager } from "../../src/key-manager/index.js";
+import rawData from "./data.js";
+import type { KeyData } from "../../src/interface.js";
+
+let data: (typeof rawData[0] & { keyManager: KeyManager } & { keyData: KeyData })[];
+
+describe("key manager", () => {
+	before(async () => {
+		data = await Promise.all(rawData.map(async r => {
+			const encodedKeyData = await generateKeyData(r.mnemonic, r.name);
+			const keyData = parseKeyData(encodedKeyData);
+			const keyManager = new KeyManager(keyData);
+
+			return { ...r, keyManager, keyData };
+		}));
+	});
+
+	it("returns the psk", () => {
+		for (const d of data) {
+			assert.deepEqual(d.keyData.psk, d.keyManager.getPskKey());
+		}
+	});
+});
