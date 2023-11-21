6ac37ec42761f02ef2aec54030fe675e6c32dc35
saul <saul@organicdesign.co.nz>
2023-11-21 13:17:32 +1300

    Fix connections command output.

--- src/rpc/commands/connections.ts
+++ src/rpc/commands/connections.ts
@@ -3,5 +3,5 @@ import type { Components } from "../../interface.js";
 export const name = "connections";
 
 export const method = (components: Components) => async () => {
-	return components.libp2p.getConnections().map(c => c.toString());
+	return components.libp2p.getConnections().map(c => c.remoteAddr.toString());
 };
