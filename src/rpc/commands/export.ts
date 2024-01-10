e34d5df6368fee260be2fc4f5e69a2f6c4c10075
saul <saul@organicdesign.co.nz>
2024-01-10 15:49:53 +1300

    Fix export command.

--- src/rpc/commands/export.ts
+++ src/rpc/commands/export.ts
@@ -1,7 +1,9 @@
+import Path from "path";
 import { z } from "zod";
 import { exportFs } from "../../fs-exporter/index.js";
 import { CID } from "multiformats/cid";
 import { decodeEntry } from "../../utils.js";
+import { DATA_KEY } from "../../interface.js"
 import { type Components, type EncodedEntry, zCID } from "../../interface.js";
 
 export const name = "export";
@@ -20,7 +22,12 @@ export const method = (components: Components) => async (raw: unknown) => {
 		throw new Error("no such group");
 	}
 
-	const encodedEntry = await group.store.selectors.get(group.store.index)(`${params.path}/ROOT`) as EncodedEntry;
+	const encodedEntry = await group.store.selectors.get(group.store.index)(Path.join(DATA_KEY, params.path)) as EncodedEntry;
+
+	if (encodedEntry == null) {
+		throw new Error("no such item");
+	}
+
 	const entry = decodeEntry(encodedEntry);
 
 	await exportFs(components, entry.cid, params.outPath);
