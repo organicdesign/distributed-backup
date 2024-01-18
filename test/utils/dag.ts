dfed8bd9892270eceb47b042f839f2d01e965349
saul <saul@organicdesign.co.nz>
2024-01-19 08:19:59 +1300

    Remove random number from dag generation.

--- test/utils/dag.ts
+++ test/utils/dag.ts
@@ -1,13 +1,19 @@
 import { fromString as uint8arrayFromString } from 'uint8arrays/from-string'
 import { addBlock } from './blocks.js'
 import * as dagPb from "@ipld/dag-pb";
-import { compare as compareUint8Arrays } from "uint8arrays/compare"
-import type { Blockstore } from 'interface-blockstore'
-import type { CID } from 'multiformats/cid'
+import { compare as compareUint8Arrays } from "uint8arrays/compare";
+import type { Blockstore } from "interface-blockstore";
+import type { CID } from "multiformats/cid";
+
+const uniqueNumber = (() => {
+	let i = 0;
+
+	return () => i++;
+})();
 
 export const createDag = async ({ blockstore }: { blockstore: Blockstore }, depth: number, children: number): Promise<CID[]> => {
 	if (depth === 0) {
-		const block = dagPb.encode({ Data: uint8arrayFromString(`level-${depth}-${Math.random()}`), Links: [] });
+		const block = dagPb.encode({ Data: uint8arrayFromString(`level-${depth}-${uniqueNumber()}`), Links: [] });
 		const cid = await addBlock({ blockstore }, block, dagPb.code);
 
 		return [cid];
@@ -22,9 +28,10 @@ export const createDag = async ({ blockstore }: { blockstore: Blockstore }, dept
 	childrenNodes.sort((a, b) => compareUint8Arrays(a[0].bytes, b[0].bytes));
 
 	const block = dagPb.encode({
-		Data: uint8arrayFromString(`level-${depth}-${Math.random()}`),
-		Links:childrenNodes.map(l => ({ Hash: l[0], Name: l[0].toString() }))
+		Data: uint8arrayFromString(`level-${depth}-${uniqueNumber()}`),
+		Links: childrenNodes.map(l => ({ Hash: l[0], Name: l[0].toString() }))
 	});
+
 	const cid = await addBlock({ blockstore }, block, dagPb.code);
 	const allBlocks = childrenNodes.reduce((a, b) => [...a, ...b], []);
 
