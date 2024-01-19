b53832917e0389c4e46fc2b21db93f3707e1ccd3
saul <saul@organicdesign.co.nz>
2024-01-19 15:42:30 +1300

    Refactor e2e test.

--- test/e2e/lifecycle.spec.ts
+++ test/e2e/lifecycle.spec.ts
@@ -1,77 +1,21 @@
-import { spawn, type ChildProcessWithoutNullStreams } from "child_process";
-import Path from "path";
-import fs from "fs/promises";
-import { toString as uint8ArrayToString } from "uint8arrays";
-import { DeferredPromise } from "@open-draft/deferred-promise";
-import { projectPath } from "../../src/utils.js";
-
-const socket = Path.join(projectPath, "test/e2e/node-a.socket");
-const tsNode = Path.join(projectPath, "node_modules/ts-node/dist/bin.js");
-
-const args = [
-	Path.join(projectPath, "src/daemon.ts"),
-	"-k", Path.join(projectPath, "test/e2e/key.json"),
-	"-c", Path.join(projectPath, "test/e2e/config.json"),
-	"-s", socket
-];
-
-const cleanSocket = async () => {
-	try {
-		await fs.rm(socket);
-	} catch {
-		// Ignore...
-	}
-}
-
-before(async () => {
-	await cleanSocket();
-});
-
-after(async () => {
-	await cleanSocket();
-});
+import runNode from "./utils/run-node.js";
 
 describe("lifecycle", () => {
-	let proc: ChildProcessWithoutNullStreams;
+	let proc: Awaited<ReturnType<typeof runNode>>;
 
-	before(() => {
-		proc = spawn(tsNode, args, { env: { ...process.env, DEBUG: "backup:*" } });
+	before(async () => {
+		proc = await runNode("lifecycle");
 	});
 
-	after(() => {
-		proc.kill(9);
+	after(async () => {
+		proc.stop();
 	});
 
-	it("outputs started after startup", async () => {
-		const promise: DeferredPromise<void> = new DeferredPromise();
-
-		const listener = (chunk: Uint8Array) => {
-			if (uint8ArrayToString(chunk).includes("started")) {
-				promise.resolve();
-			}
-		};
-
-		proc.stderr.on("data", listener);
-
-		await promise;
-
-		proc.stderr.off("data", listener);
+	it("starts cleanly", async () => {
+		await proc.start();
 	});
 
-	it("outputs ended after sigint", async () => {
-		const promise: DeferredPromise<void> = new DeferredPromise();
-
-		const listener = (chunk: Uint8Array) => {
-			if (uint8ArrayToString(chunk).includes("exiting...")) {
-				promise.resolve();
-			}
-		};
-
-		proc.stderr.on("data", listener);
-		proc.kill("SIGINT");
-
-		await promise;
-
-		proc.stderr.off("data", listener);
+	it("stops cleanly", async () => {
+		await proc.stop();
 	});
 });
