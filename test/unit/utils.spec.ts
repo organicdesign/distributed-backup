36d34cca9f22767a30c8792fb9c66769a5e10268
saul <saul@organicdesign.co.nz>
2024-01-22 16:19:06 +1300

    Add tests for encoding and decoding data.

--- test/unit/utils.spec.ts
+++ test/unit/utils.spec.ts
@@ -4,7 +4,14 @@ import { CID } from "multiformats/cid";
 import * as raw from "multiformats/codecs/raw";
 import { sha256 } from "multiformats/hashes/sha2";
 import { MEMORY_MAGIC } from "../../src/interface.js";
-import { isMemory, safePin, safeUnpin, safeReplace } from "../../src/utils.js";
+import {
+	isMemory,
+	safePin,
+	safeUnpin,
+	safeReplace,
+	encodeAny,
+	decodeAny
+} from "../../src/utils.js";
 
 describe("isMemory", () => {
 	it("returns true if the memory magic is passed", () => {
@@ -117,5 +124,41 @@ describe("safe pinning", () => {
 		}
 
 		assert(await helia.pins.isPinned(rCid));
+	});
+});
+
+describe("cbor encoding and decoding", () => {
+	const data = [
+		{
+			decoded: new Uint8Array([0, 1, 2, 3]),
+			encoded: new Uint8Array([68, 0, 1, 2, 3])
+		},
+
+		{
+			decoded: "str",
+			encoded: new Uint8Array([99, 115, 116, 114])
+		},
+
+		{
+			decoded: 9999,
+			encoded: new Uint8Array([25, 39, 15])
+		},
+
+		{
+			decoded: [ { test: "value" } ],
+			encoded: new Uint8Array([129, 161, 100, 116, 101, 115, 116, 101, 118, 97, 108, 117, 101])
+		}
+	];
+
+	it("encodes any data", () => {
+		for (const { encoded, decoded } of data) {
+			assert.deepEqual(encoded, encodeAny(decoded));
+		}
+	});
+
+	it("decodes any data", () => {
+		for (const { encoded, decoded } of data) {
+			assert.deepEqual(decodeAny(encoded), decoded);
+		}
 	});
 });
