9bac81c8ad29724c4d78ca8eed190744ba75ce69
saul <saul@organicdesign.co.nz>
2024-01-22 16:02:19 +1300

    Add tests for safe pinning.

--- test/unit/utils.spec.ts
+++ test/unit/utils.spec.ts
@@ -1,6 +1,10 @@
 import assert from "assert/strict";
+import { createHelia, type Helia  } from "helia";
+import { CID } from "multiformats/cid";
+import * as raw from "multiformats/codecs/raw";
+import { sha256 } from "multiformats/hashes/sha2";
 import { MEMORY_MAGIC } from "../../src/interface.js";
-import { isMemory } from "../../src/utils.js";
+import { isMemory, safePin, safeUnpin } from "../../src/utils.js";
 
 describe("isMemory", () => {
 	it("returns true if the memory magic is passed", () => {
@@ -19,6 +23,69 @@ describe("isMemory", () => {
 
 		for (const value of values) {
 			assert(!isMemory(value));
+		}
+	});
+});
+
+describe("safe pinning", () => {
+	const blockData = [
+		new Uint8Array([]),
+		new Uint8Array([0, 1, 2, 3]),
+		new Uint8Array([255, 255, 255, 255])
+	];
+
+	const cids: CID[] = [];
+
+	let helia: Helia;
+
+	beforeEach(async () => {
+		helia = await createHelia();
+
+		for (const data of blockData) {
+			const digest = await sha256.digest(data);
+			const cid = CID.createV1(raw.code, digest);
+
+			await helia.blockstore.put(cid, data);
+
+			cids.push(cid);
+		}
+	});
+
+	afterEach(async () => {
+		await helia.stop();
+	});
+
+	it("safePin pins cids", async () => {
+		await Promise.all(cids.map(cid => safePin(helia, cid)));
+
+		for (const cid of cids) {
+			assert(await helia.pins.isPinned(cid));
+		}
+	});
+
+	it("safePin does not throw if the cid is already pinned", async () => {
+		await Promise.all(cids.map(cid => helia.pins.add(cid)));
+		await Promise.all(cids.map(cid => safePin(helia, cid)));
+
+		for (const cid of cids) {
+			assert(await helia.pins.isPinned(cid));
+		}
+	});
+
+	it("safeUnpin unpins a cid", async () => {
+		await Promise.all(cids.map(cid => helia.pins.add(cid)));
+		await Promise.all(cids.map(cid => safeUnpin(helia, cid)));
+
+		for (const cid of cids) {
+			assert(!await helia.pins.isPinned(cid));
+		}
+	});
+
+	it("safeUnpin does not throw an error if the cid is not pinned", async () => {
+		await Promise.all(cids.map(cid => safeUnpin(helia, cid)));
+
+		for (const cid of cids) {
+			assert(!await helia.pins.isPinned(cid));
 		}
 	});
 });
