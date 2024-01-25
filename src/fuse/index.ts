c405f1a282d229355cd8b37b7d6242c07e0372ec
saul <saul@organicdesign.co.nz>
2024-01-25 18:23:27 +1300

    Add typings.

--- src/fuse/index.ts
+++ src/fuse/index.ts
@@ -2,6 +2,38 @@ import { promisify } from "util";
 import Fuse from "@cocalc/fuse-native";
 import { createNetClient, NetClient } from "@organicdesign/net-rpc";
 
+interface Stat {
+	mtime: Date
+	atime: Date
+	ctime: Date
+	size: number
+	mode: number
+	uid: number
+	gid: number
+	nlink: number
+	dev: number
+	blksize: number
+	blocks: number
+	ino: number
+	rdev: number
+}
+
+interface FuseCBOpts {
+	readdir (path: string, cb: (err: number, names: string[], stats?: Fuse.Stats[]) => void): void
+	getattr (path: string, cb: (err: number, stats?: Fuse.Stats) => void): void
+	open (path: string, mode: number, cb: (err: number, fd: number) => void): void
+	release (path: string, fd: number, cb: (err: number) => void): void
+	read (path: string, fd: number, bufffer: Buffer, length: number, position: number, cb: (bytesRead: number) => void): void
+}
+
+interface FuseOpts {
+	readdir (path: string): Promise<string[]>
+	getattr (path: string): Promise<Fuse.Stats>
+	open (path: string, mode: number): Promise<number>
+	release (path: string, fd: number): Promise<void>
+	read (path: string, fd: number, bufffer: Buffer, length: number, position: number): Promise<void>
+}
+
 const stat = (st: {
 	mtime?: Date
 	atime?: Date
@@ -10,15 +42,15 @@ const stat = (st: {
 	mode: string | number,
 	uid?: number
 	gid?: number
-}) => {
+}): Stat => {
 	return {
 		mtime: st.mtime || new Date(),
 		atime: st.atime || new Date(),
 		ctime: st.ctime || new Date(),
-		size: st.size !== undefined ? st.size : 0,
+		size: st.size ?? 0,
 		mode: st.mode === 'dir' ? 16877 : (st.mode === 'file' ? 33188 : (st.mode === 'link' ? 41453 : 0)),
-		uid: st.uid !== undefined ? st.uid : process.getuid?.() ?? 0,
-		gid: st.gid !== undefined ? st.gid : process.getgid?.() ?? 0,
+		uid: st.uid ?? process.getuid?.() ?? 0,
+		gid: st.gid ?? process.getgid?.() ?? 0,
 		nlink: 0,
 		dev: 0,
 		blksize: 0,
@@ -28,46 +60,63 @@ const stat = (st: {
 	}
 };
 
-const opts = {
+const opts: FuseOpts = {
 	async readdir (path: string) {
-		if (path === '/') return ['test'];
+		if (path === '/') {
+			return ['test'];
+		}
+
 		throw Fuse.ENOENT
 	},
 
 	async getattr (path) {
-		if (path === '/') return stat({ mode: 'dir', size: 4096 });
-		if (path === '/test') return stat({ mode: 'file', size: 11 });
+		if (path === '/') {
+			return stat({ mode: 'dir', size: 4096 });
+		}
+
+		if (path === '/test') {
+			return stat({ mode: 'file', size: 11 });
+		}
+
 		throw Fuse.ENOENT;
 	},
 
-	async open (path, flags, cb) {
-		return 42
+	async open () {
+		return 42;
 	},
 
-	async release (path, fd, cb) {
-		return;
-	},
+	async release () {},
 
-	async read (path, fd, buf, len, pos, cb) {
-		var str = 'hello world'.slice(pos, pos + len)
-		if (!str) return;
-		buf.write(str)
+	//@ts-ignore
+	async read (_, __, buf, len, pos) {
+		const str = 'hello world'.slice(pos, pos + len)
+
+		if (str.length !== 0) {
+			buf.write(str)
+		};
+
 		return str.length
 	}
 };
 
-const out = {};
+const toCallbacks = (opts: FuseOpts): FuseCBOpts => {
+	const out: Partial<FuseCBOpts> = {};
 
-for (const key of Object.keys(opts)) {
-	out[key] = function () {
-		const args = [...arguments]
-		const cb = args.pop();
+	for (const key of Object.keys(opts)) {
+		out[key] = function () {
+			const args = [...arguments]
+			const cb = args.pop();
 
-		opts[key].apply(fuse, args).then(r => cb(0, r)).catch(e => cb(e));
+			opts[key].apply(fuse, args).then(function (r: unknown) {
+				cb(0, r);
+			}).catch((e: number) => cb(e));
+		}
 	}
-}
 
-const fuse = new Fuse("/tmp/fuse", out, { debug: true });
+	return out as FuseCBOpts;
+};
+
+const fuse = new Fuse("/tmp/fuse", toCallbacks(opts), { debug: true });
 
 process.on("uncaughtException", async (error) => {
 	console.error("PANIC!", error);
