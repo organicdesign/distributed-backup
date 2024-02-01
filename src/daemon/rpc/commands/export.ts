bf87a619ede0c52866fe4a3b2833e9149c4debb8
saul <saul@organicdesign.co.nz>
2024-02-01 14:52:13 +1300

    Get export to work recursively.

--- src/daemon/rpc/commands/export.ts
+++ src/daemon/rpc/commands/export.ts
@@ -1,10 +1,11 @@
 import Path from "path";
 import { z } from "zod";
+import * as dagCbor from "@ipld/dag-cbor";
 import { fsExport } from "../../../fs-import-export/index.js";
 import { CID } from "multiformats/cid";
 import { decodeEntry } from "../../utils.js";
 import { DATA_KEY } from "../../interface.js"
-import { type Components, type EncodedEntry, zCID } from "../../interface.js";
+import { type Components, EncodedEntry, zCID } from "../../interface.js";
 
 export const name = "export";
 
@@ -16,19 +17,24 @@ const Params = z.object({
 
 export const method = (components: Components) => async (raw: unknown) => {
 	const params = Params.parse(raw);
-	const group = components.groups.get(CID.parse(params.group));
+	const database = components.groups.get(CID.parse(params.group));
 
-	if (group == null) {
+	if (database == null) {
 		throw new Error("no such group");
 	}
 
-	const encodedEntry = await group.store.selectors.get(group.store.index)(Path.join(DATA_KEY, params.path)) as EncodedEntry;
+	const index = await database.store.latest();
 
-	if (encodedEntry == null) {
-		throw new Error("no such item");
+	for await (const pair of index.query({ prefix: Path.join("/", DATA_KEY, params.path)})) {
+		const encodedEntry = EncodedEntry.parse(dagCbor.decode(pair.value));
+
+		if (encodedEntry == null) {
+			continue;
+		}
+
+		const entry = decodeEntry(encodedEntry);
+		const virtualPath = pair.key.toString().replace("/r", "");
+
+		await fsExport(components.blockstore, Path.join(params.outPath, virtualPath.replace(params.path, "")), entry.cid);
 	}
-
-	const entry = decodeEntry(encodedEntry);
-
-	await fsExport(components.blockstore, params.outPath, entry.cid);
 };
