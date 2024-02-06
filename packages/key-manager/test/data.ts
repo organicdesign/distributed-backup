83b0cb3ad02b31bd540f2b39e8ff0c3c04807506
saul <saul@organicdesign.co.nz>
2024-02-06 14:55:26 +1300

    Get key manager tests running again.

---
+++ packages/key-manager/test/data.ts
@@ -0,0 +1,41 @@
+export default [
+  {
+    mnemonic: 'anxiety rib chief game raw skin awkward arrow answer oppose way leopard',
+    name: 'test',
+    key: '5TP9VimJU1WdSoTxZGLhSuPKqCpXirPHDK4ZjHxzetex-8zAV14C4oLe4dytUSVzznTuQ659pY1dSMG8HAQenDqVQ',
+    publicKey: '28uzwJB5KrEXQE6s6HgYAgQCLFkpuJoRTary7hKzAmcXh-8zAV14C4oLe4dytUSVzznTuQ659pY1dSMG8HAQenDqVQ',
+    psk: '/key/swarm/psk/1.0.0/\n/base16/\n56d3c18282f1f1b1b3e04e40dd5d8bf44cafa8bc9c9bc7c57716a7766fa2c550'
+  },
+
+  {
+    mnemonic: 'hedgehog symbol trumpet rely strong eight property keen rose legend trigger mutual',
+    name: 'test2',
+    key: '8ZFp4wWD4SoJvuBdCAqAjJG5cSSoECJrevqJWfUjQCEi-8yf9PQFFydhXHGJ7ziX57jE533eKHQ8vSqgucLggXzt2',
+    publicKey: '23PFvgTSohdTBjh5NxnBsz9RKBX1ahkhNSLCb83aDyjWV-8yf9PQFFydhXHGJ7ziX57jE533eKHQ8vSqgucLggXzt2',
+    psk: '/key/swarm/psk/1.0.0/\n/base16/\n34d1809075d4947f628abecdcea877bd2ff29bb441db608487afbf7136313a22'
+  },
+
+  {
+    mnemonic: 'brave exact wonder grape theory salon arrive party rail cargo saddle height',
+    name: 'server',
+    key: '5sj1C5eoUNKizJtusTc7Kzs7EVdkZK61mtHWevp6oVwD-DFs2kfoWCYeUQahLQNKDZiezzqUomtA43rUHWNG9112Y',
+    publicKey: '25fju6xfyY9jFuFLFgQSmt9RHzre21P8gmYSC92kK67iq-DFs2kfoWCYeUQahLQNKDZiezzqUomtA43rUHWNG9112Y',
+    psk: '/key/swarm/psk/1.0.0/\n/base16/\n6eaf13c66b04c37c4ea14381a6dc691843aa2f8e9e979b8df5e8c9c2a88c840e'
+  },
+
+  {
+    mnemonic: 'divert begin hedgehog zone alien wheel victory eagle matter enroll one inch',
+    name: 'a long name with spaces',
+    key: 'Dgqny1ify4jdmCTFiGYznnaRMNk9ASPe11719ffwQwyU-CN1b67hBtzeWrea1ykh8NUWk3s5bHhixSKdwM1WSUy1D',
+    publicKey: 'whjdDyjaCDKWgLnSLZFsq87CGEkn5wT4cg7j5KTgxZfB-CN1b67hBtzeWrea1ykh8NUWk3s5bHhixSKdwM1WSUy1D',
+    psk: '/key/swarm/psk/1.0.0/\n/base16/\n79fba3ed8d5eadf8f8d5805321db83b63a64e7be178aee2b088f6184fa4189fa'
+  },
+
+  {
+    mnemonic: 'bracket raise onion simple hybrid dignity follow observe shoulder bachelor stadium congress',
+    name: '1234567890-=@#$%^&*()_+"\':;,./<?>',
+    key: 'wiJxUYpkAu2swgpaYqRANFXNx6qLGunVQ1E13yJ8ex7-4BKoMvWn2RkxXJzQNnH529aonQ1tLroBYKAkqgjery8B',
+    publicKey: 'pjbo9WmANYms6WebNJeZTT3NSLARNaeSV1XobPxjzbWY-4BKoMvWn2RkxXJzQNnH529aonQ1tLroBYKAkqgjery8B',
+    psk: '/key/swarm/psk/1.0.0/\n/base16/\n6f3b813e892454b6435e68c51e905d4d707518f7ed80a4f6c3009a2441db4fe2'
+  }
+]
