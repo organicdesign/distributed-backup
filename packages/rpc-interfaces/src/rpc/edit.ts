a83b2e4d7bd63aa4f9bac6ef728b125c898c06e1
saul <saul@organicdesign.co.nz>
2024-02-12 09:54:03 +1300

    Change edit return type.

--- packages/rpc-interfaces/src/rpc/edit.ts
+++ packages/rpc-interfaces/src/rpc/edit.ts
@@ -13,7 +13,7 @@ export const Params = z.object({
 // eslint-disable-next-line @typescript-eslint/no-redeclare
 export type Params = z.input<typeof Params>
 
-export const Return = z.array(z.string())
+export const Return = z.null()
 
 // eslint-disable-next-line @typescript-eslint/no-redeclare
 export type Return = z.infer<typeof Return>
