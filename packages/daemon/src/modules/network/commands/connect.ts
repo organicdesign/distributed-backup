da6e389235da43cf61a678f2aefa07f28684ff5b
saul <saul@organicdesign.co.nz>
2024-02-20 08:58:45 +1300

    Simplify the connect command.

--- packages/daemon/src/modules/network/commands/connect.ts
+++ packages/daemon/src/modules/network/commands/connect.ts
@@ -3,19 +3,17 @@ import { Connect } from 'rpc-interfaces'
 import type { Provides } from '../index.js'
 import type { RPCCommandConstructor } from '@/interface.js'
 
-const command: RPCCommandConstructor<Provides> = (context) => {
-  return {
-    name: Connect.name,
+const command: RPCCommandConstructor<Provides> = (context) => ({
+  name: Connect.name,
 
-    async method (raw: unknown): Promise<Connect.Return> {
-      const params = Connect.Params.parse(raw)
-      const address = multiaddr(params.address)
+  async method (raw: unknown): Promise<Connect.Return> {
+    const params = Connect.Params.parse(raw)
+    const address = multiaddr(params.address)
 
-      await context.libp2p.dial(address)
+    await context.libp2p.dial(address)
 
-      return null
-    }
+    return null
   }
-}
+})
 
 export default command
