e62f6f371d45916022f6b72b5c7f642ae5b7bebb
saul <saul@organicdesign.co.nz>
2024-02-22 10:44:12 +1300

    Add group to set priority command.

--- packages/daemon/src/modules/downloader/commands/set-priority.ts
+++ packages/daemon/src/modules/downloader/commands/set-priority.ts
@@ -1,16 +1,20 @@
+import Path from 'path'
+import { zCID } from 'rpc-interfaces/zod'
 import { z } from 'zod'
 import type { Provides, Requires } from '../index.js'
 import type { ModuleMethod } from '@/interface.js'
 
 const Params = z.object({
   priority: z.number().int().min(1).max(100),
+  group: zCID(),
   path: z.string()
 })
 
 const command: ModuleMethod<Provides, Requires> = (context, { rpc }) => {
   rpc.addMethod('set-priority', async (raw: unknown) => {
     const params = Params.parse(raw)
-    const pinInfo = await context.pinManager.get(params.path)
+    const key = Path.join('/', params.group, params.path)
+    const pinInfo = await context.pinManager.get(key)
 
     if (pinInfo == null) {
       throw new Error('no such pin')
@@ -18,7 +22,7 @@ const command: ModuleMethod<Provides, Requires> = (context, { rpc }) => {
 
     pinInfo.priority = params.priority
 
-    await context.pinManager.put(params.path, pinInfo)
+    await context.pinManager.put(key, pinInfo)
 
     return null
   })
