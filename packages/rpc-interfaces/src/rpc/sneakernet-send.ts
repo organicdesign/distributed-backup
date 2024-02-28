30704fc1f380c8688010cb58136ffea507290df9
saul <saul@organicdesign.co.nz>
2024-02-28 15:45:13 +1300

    Add sneakernet commands to rpc-interfaces.

---
+++ packages/rpc-interfaces/src/rpc/sneakernet-send.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+import { zEncoding } from '../zod.js'
+
+export const name = 'sneakernet-send'
+
+export const Params = z.object({
+  path: z.string(),
+  peers: z.array(zEncoding('base58btc'))
+})
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export type Params = z.input<typeof Params>
+
+export const Return = z.null()
+
+// eslint-disable-next-line @typescript-eslint/no-redeclare
+export type Return = z.infer<typeof Return>
