addb50e6b15861bd39bcda585b89f9107784ce7d
saul <saul@organicdesign.co.nz>
2024-03-05 15:29:05 +1300

    Remove redundant interfaces.

--- packages/daemon/src/modules/packages/interface.ts
+++ packages/daemon/src/modules/packages/interface.ts
@@ -1,18 +1,1 @@
-import { z } from 'zod'
-import type { CID } from 'multiformats/cid'
-
 export const PACKAGE_KEY = '.packages'
-
-export const EncodedEntry = z.union([
-  z.object({
-    cid: z.instanceof(Uint8Array)
-  }),
-  z.null()
-])
-
-// eslint-disable-next-line @typescript-eslint/no-redeclare
-export type EncodedEntry = z.infer<typeof EncodedEntry>
-
-export type Entry = {
-  [P in keyof NonNullable<EncodedEntry>]: NonNullable<EncodedEntry>[P] extends Uint8Array ? CID : NonNullable<EncodedEntry>[P]
-}
