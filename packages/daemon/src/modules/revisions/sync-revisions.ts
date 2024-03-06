a57ed130f2811479fcb6119561b5d47f9dec7772
saul <saul@organicdesign.co.nz>
2024-03-06 15:43:54 +1300

    Fix possible null error in revision tracker.

--- packages/daemon/src/modules/revisions/sync-revisions.ts
+++ packages/daemon/src/modules/revisions/sync-revisions.ts
@@ -1,24 +1,26 @@
 import Path from 'path'
 import * as dagCbor from '@ipld/dag-cbor'
-import { VERSION_KEY } from './interface.js'
+import { VERSION_KEY, EncodedEntry } from './interface.js'
 import { decodeEntry } from './utils.js'
 import { type Requires, logger } from './index.js'
 
 export default async ({ groups, downloader }: Requires): Promise<void> => {
   for (const { value: database } of groups.groups.all()) {
     const tracker = groups.getTracker(database)
+    const group = database.manifest.address.cid
 
     for await (const { key, value } of tracker.process(`/${VERSION_KEY}`)) {
-      const entry = decodeEntry(dagCbor.decode(value))
-      const group = database.manifest.address.cid
+      const data = EncodedEntry.parse(dagCbor.decode(value))
       const fullKey = Path.join('/', group.toString(), key)
 
-      logger.info('[entry] syncing update:', fullKey)
-
-      if (entry == null) {
+      if (data == null) {
         await downloader.pinManager.remove(fullKey)
         continue
       }
+
+      const entry = decodeEntry(data)
+
+      logger.info('[entry] syncing update:', fullKey)
 
       await downloader.pinManager.put(fullKey, { cid: entry.cid, priority: entry.priority })
     }
