6e5d2a4c950022e1b90d70f1e5655e340ba89757
saul <saul@organicdesign.co.nz>
2024-03-15 13:58:53 +1300

    Test retrieve aborting.

--- packages/manual-block-broker/test/index.spec.ts
+++ packages/manual-block-broker/test/index.spec.ts
@@ -4,15 +4,29 @@ import { ManualBlockBroker } from '../src/index.js'
 
 const cid = CID.parse('QmbFMke1KXqnYyBBWxB74N4c5SBnJMVAiMNRcGu6x1AwQH')
 
+// Since the block broker does not verify the block it doesn't need to match CID.
+const block = new Uint8Array()
+
 describe('manual block broker', () => {
-  it('retrieve resolves  when a block is provided', async () => {
+  it('retrieve resolves when a block is provided', async () => {
     const bb = new ManualBlockBroker()
 
     const promise = bb.retrieve(cid, {})
-    const result = bb.provide(cid, new Uint8Array())
+    const result = bb.provide(cid, block)
 
     assert(result)
 
     await promise
+  })
+
+  it('retrieve throws when aborted', async () => {
+    const bb = new ManualBlockBroker()
+
+    const controller = new AbortController()
+    const promise = bb.retrieve(cid, { signal: controller.signal })
+
+    controller.abort()
+
+    await assert.rejects(async () => promise)
   })
 })
