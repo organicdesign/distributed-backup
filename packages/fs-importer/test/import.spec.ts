afc2124b47c50eb72994f8eda02d0a5885ad8855
saul <saul@organicdesign.co.nz>
2024-03-19 10:10:02 +1300

    Cleanup test.

--- packages/fs-importer/test/import.spec.ts
+++ packages/fs-importer/test/import.spec.ts
@@ -1,5 +1,4 @@
 import assert from 'assert/strict'
-import fs from 'fs/promises'
 import Path from 'path'
 import { fileURLToPath } from 'url'
 import { BlackHoleBlockstore } from 'blockstore-core/black-hole'
@@ -9,44 +8,52 @@ import { importer, selectChunker, selectHasher } from '../src/index.js'
 
 const dataPath = Path.join(Path.dirname(fileURLToPath(import.meta.url)), '../../test-data')
 
+const blockstore = new BlackHoleBlockstore()
+
+const importerConfig = {
+	chunker: selectChunker(),
+	hasher: selectHasher(),
+	cidVersion: 1
+} as const
+
+const expectedData = [
+  {
+    cid: CID.parse('bafybeihoqexapn3tusc4rrkqztzzemz7y57esnzg7eutsua4ehjkylmjqe'),
+    path: Path.join(dataPath, 'file-1.txt'),
+    size: 447
+  },
+  {
+    cid: CID.parse('bafybeibac7pp5mcxkj7s55bjdbr7tj3pj7col4janvm36y4fjvxqs67fsi'),
+    path: Path.join(dataPath, 'file-2.txt'),
+    size: 1791
+  },
+  {
+    cid: CID.parse('bafybeihxa6uyvmdl6wdjxnwpluocix2csrq3ifunemjr2jxy35wjkl2v64'),
+    path: Path.join(dataPath, 'dir-1/file-3.txt'),
+    size: 45
+  }
+]
+
 describe('importer', () => {
-  it('imports a file ', async () => {
-    const path = Path.join(dataPath, 'file-1.txt')
-    const cid = CID.parse('bafybeihoqexapn3tusc4rrkqztzzemz7y57esnzg7eutsua4ehjkylmjqe')
-    const { size } = await fs.stat(path)
+  it('imports files ', async () => {
+    for (const data of expectedData) {
+      const results = await all(importer(
+        blockstore,
+        data.path,
+        importerConfig
+      ))
 
-    const results = await all(importer(
-      new BlackHoleBlockstore(),
-      path,
-      { chunker: selectChunker(), hasher: selectHasher(), cidVersion: 1 }
-    ))
-
-    assert.deepEqual(results, [{ cid, path, size }])
+      assert.deepEqual(results, [data])
+    }
   })
 
   it('imports a directory ', async () => {
     const results = await all(importer(
-      new BlackHoleBlockstore(),
+      blockstore,
       dataPath,
-      { chunker: selectChunker(), hasher: selectHasher(), cidVersion: 1 }
+      importerConfig
     ))
 
-    assert.deepEqual(results, [
-      {
-        cid: CID.parse('bafybeihoqexapn3tusc4rrkqztzzemz7y57esnzg7eutsua4ehjkylmjqe'),
-        path: Path.join(dataPath, 'file-1.txt'),
-        size: 447
-      },
-      {
-        cid: CID.parse('bafybeibac7pp5mcxkj7s55bjdbr7tj3pj7col4janvm36y4fjvxqs67fsi'),
-        path: Path.join(dataPath, 'file-2.txt'),
-        size: 1791
-      },
-      {
-        cid: CID.parse('bafybeihxa6uyvmdl6wdjxnwpluocix2csrq3ifunemjr2jxy35wjkl2v64'),
-        path: Path.join(dataPath, 'dir-1/file-3.txt'),
-        size: 45
-      }
-    ])
+    assert.deepEqual(results, expectedData)
   })
 })
