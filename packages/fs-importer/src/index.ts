806ae557d1c5526b3bad15c53abcf11e0b5edd07
saul <saul@organicdesign.co.nz>
2024-03-19 09:36:36 +1300

    Disable legacy importers.

--- packages/fs-importer/src/index.ts
+++ packages/fs-importer/src/index.ts
@@ -1,5 +1,5 @@
-import importEncryptedFunc from './import-encrypted.js'
-import importPlaintextFunc from './import-plaintext.js'
+// import importEncryptedFunc from './import-encrypted.js'
+// import importPlaintextFunc from './import-plaintext.js'
 import importRecursiveFunc from './import-recursive.js'
 import selectChunkerFunc from './select-chunker.js'
 import selectHasherFunc from './select-hasher.js'
@@ -8,6 +8,7 @@ export type * from './interfaces.js'
 
 export const selectHasher = selectHasherFunc
 export const selectChunker = selectChunkerFunc
-export const importPlaintext = importPlaintextFunc
-export const importEncrypted = importEncryptedFunc
-export const importRecursive = importRecursiveFunc
+// No longer want to support these methods, keeping them here for reference.
+// export const importPlaintext = importPlaintextFunc
+// export const importEncrypted = importEncryptedFunc
+export const importer = importRecursiveFunc
