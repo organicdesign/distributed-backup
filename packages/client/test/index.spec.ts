1ba894ed5ead8888a1f46ccb7e13ec2c3ea274f3
saul <saul@organicdesign.co.nz>
2024-03-19 15:18:23 +1300

    Test write.

--- packages/client/test/index.spec.ts
+++ packages/client/test/index.spec.ts
@@ -995,4 +995,40 @@ describe('addresses', () => {
     assert.deepEqual(req, {})
     assert.deepEqual(res, null)
   })
+
+  it('handles write requests/responses', async () => {
+    const requests = [
+      {
+        params: {
+          group: 'QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+          path: '/test',
+          data: ''
+        },
+
+        response: 0
+      },
+
+      {
+        params: {
+          group: 'QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ',
+          path: '/my/file',
+          position: 100,
+          length: 256,
+          data: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        },
+
+        response: 256
+      }
+    ]
+
+    for (const { params, response } of requests) {
+      const [req, res] = await Promise.all([
+        getRequest(interfaces.Write.name, async () => response),
+        client.write(params.group, params.path, params.data, params)
+      ])
+
+      assert.deepEqual(req, params)
+      assert.deepEqual(res, response)
+    }
+  })
 })
