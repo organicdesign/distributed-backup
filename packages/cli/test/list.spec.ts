cf4708803a19c153e257b047d074c3ab222ea40f
saul <saul@organicdesign.co.nz>
2024-03-20 12:57:50 +1300

    Test list command.

---
+++ packages/cli/test/list.spec.ts
@@ -0,0 +1,89 @@
+import assert from 'assert/strict'
+import { handler } from '../src/commands/list.js'
+import { mockParams } from './utils.js'
+
+describe('list', () => {
+  const cid = 'QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN'
+
+  const items = [
+    {
+      name: 'file',
+      group: 'QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ',
+      path: '/my-dir/file',
+      cid,
+      priority: 1,
+      blocks: 50,
+      size: 500,
+      timestamp: 123456,
+      encrypted: false,
+      author: 'GZsJqUmyVjBm8bk7Gkdb3MVTspKUYYn1P5hriJMnxkahxp9jpi',
+      revisionStrategy: 'all' as const
+    }
+  ]
+
+  it('text', async () => {
+    const params = mockParams({ list: items, countPeers: [{ cid, peers: 1 }], getStatus: [{ cid, state: 'COMPLETED', size: 50, blocks: 5 }], getSpeeds: [{ cid, speed: 1000 }] }, { group: 'group-abc' })
+
+    const response = await handler(params)
+
+    let expected = 'Name'.padEnd(20)
+
+    expected += 'Size'.padEnd(27)
+    expected += 'Speed'.padEnd(27)
+    expected += 'Blocks'.padEnd(20)
+    expected += 'State'.padEnd(15)
+    expected += 'Priority'.padEnd(10)
+    expected += 'Revisions'.padEnd(10)
+    expected += 'Peers'.padEnd(10)
+    expected += 'Group'.padEnd(10)
+    expected += 'Encrypted'.padEnd(10)
+    expected += 'R-Strategy'.padEnd(12)
+    expected += 'CID'.padEnd(62)
+    expected += '\n'
+    expected += '/\n'
+    expected += '  my-dir/\n'
+    expected += '    file'
+    expected += '            50 B/500 B (10%)'
+    expected += '           1000 KB/s (1 s)'
+    expected += '            5/50 (10%)'
+    expected += '          COMPLETED'
+    expected += '      1         0         1'
+    expected += '         QmaCpDMG  false     all'
+    expected += '         QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN'
+    expected += '                \n\n'
+    expected += 'Total'
+    expected += '          Size'
+    expected += '                     Blocks'
+    expected += '              Speed'
+    expected += '               \n'
+    expected += '1/1 (100%)'
+    expected += '     50 B/500 B (10%)'
+    expected += '         5/50 (10%)'
+    expected += '          1000 KBs            '
+
+    assert.equal(response, expected)
+  })
+
+  it('json', async () => {
+    const params = mockParams({
+      list: items,
+      countPeers: [{ cid, peers: 1 }],
+      getStatus: [{ cid, state: 'COMPLETED', size: 50, blocks: 5 }],
+      getSpeeds: [{ cid, speed: 1000 }]
+    }, {
+      group: 'group-abc',
+      json: true
+    })
+
+    const response = await handler(params)
+
+    assert.equal(response, JSON.stringify({
+      items,
+      total: {
+        blocks: 50,
+        size: 500,
+        count: 1
+      }
+    }))
+  })
+})
