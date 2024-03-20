fcbbe2e240a8eef8b828de0ccd470df3e463e96a
saul <saul@organicdesign.co.nz>
2024-03-21 09:48:58 +1300

    Test sigint.

---
+++ packages/daemon/test/modules/sigint.spec.ts
@@ -0,0 +1,15 @@
+import assert from 'assert/strict'
+import sigint from '../../src/modules/sigint/index.js'
+
+describe('sigint', () => {
+  it('calls the interupt method when interupt gets called', async () => {
+    const m = await sigint()
+
+    let called = 0
+
+    m.onInterupt(() => called++)
+    m.interupt()
+
+    assert.equal(called, 1)
+  })
+})
