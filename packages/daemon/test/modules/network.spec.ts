3329fc5ad9b9fe6e16676f10ce2c71b4203e5c31
saul <saul@organicdesign.co.nz>
2024-03-21 16:30:01 +1300

    Test network config.

---
+++ packages/daemon/test/modules/network.spec.ts
@@ -0,0 +1,51 @@
+import assert from 'assert/strict'
+import fs from 'fs/promises'
+import network from '../../src/modules/network/index.js'
+import createRpc from '../../src/modules/rpc/index.js'
+import createSigint from '../../src/modules/sigint/index.js'
+import { mkTestPath } from '../utils/paths.js'
+import mockArgv from './mock-argv.js'
+import mockBase from './mock-base.js'
+import mockConfig from './mock-config.js'
+import type { Requires as NetworkComponents } from '../../src/modules/network/index.js'
+
+describe('network', () => {
+  const testPath = mkTestPath('network')
+  let components: NetworkComponents
+
+  before(async () => {
+    await fs.mkdir(testPath, { recursive: true })
+
+    const sigint = await createSigint()
+    const config = mockConfig({ storage: ':memory:' })
+    const argv = mockArgv(testPath)
+    const base = mockBase()
+    const rpc = await createRpc({ argv, sigint })
+
+    components = {
+      sigint,
+      config,
+      base,
+      rpc
+    }
+  })
+
+  after(async () => {
+    await fs.rm(testPath, { recursive: true })
+
+    components.sigint.interupt()
+  })
+
+  it('provides defaults for the config options', async () => {
+    const m = await network(components)
+
+    assert.deepEqual(m.config.addresses, [
+      '/ip4/127.0.0.1/tcp/0',
+      '/ip4/127.0.0.1/tcp/0/ws'
+    ])
+
+    assert.deepEqual(m.config.bootstrap, [])
+    assert.equal(m.config.private, false)
+    assert.equal(m.config.serverMode, false)
+  })
+})
