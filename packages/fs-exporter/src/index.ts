8e93507689117b3c4a384267c844556a01a050d8
saul <saul@organicdesign.co.nz>
2024-03-26 16:22:57 +1300

    Rework the exporter to work of the ipfs exporter.

--- packages/fs-exporter/src/index.ts
+++ packages/fs-exporter/src/index.ts
@@ -1,3 +1,26 @@
-import exportPlaintextFunc from './export-plaintext.js'
+import fs from 'fs/promises'
+import Path from 'path'
+import { exporter as ufsExporter } from 'ipfs-unixfs-exporter'
+import type { Blockstore } from 'interface-blockstore'
+import type { CID } from 'multiformats/cid'
 
-export const exporter = exportPlaintextFunc
+export const exporter = async (blockstore: Blockstore, path: string, cid: CID): Promise<void> => {
+  const e = await ufsExporter(cid, blockstore)
+
+  if (e.type === 'file') {
+    await fs.mkdir(path, { recursive: true })
+    await fs.writeFile(Path.join(path, e.name), e.content())
+    return
+  }
+
+  if (e.type === 'raw') {
+    await fs.mkdir(Path.join(path, '..'), { recursive: true })
+    await fs.writeFile(path, e.content())
+  }
+
+  if (e.type === 'directory') {
+    for (const link of e.node.Links) {
+      await exporter(blockstore, Path.join(path, link.Name ?? ''), link.Hash)
+    }
+  }
+}
