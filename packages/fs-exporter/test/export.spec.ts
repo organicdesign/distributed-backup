94dd1881136f078ed7d975d22445822f7d0075c6
saul <saul@organicdesign.co.nz>
2024-03-26 17:36:11 +1300

    Refactor export test with test data package.

--- packages/fs-exporter/test/export.spec.ts
+++ packages/fs-exporter/test/export.spec.ts
@@ -1,40 +1,16 @@
 import assert from 'assert/strict'
-import { createHash } from 'crypto'
 import fs from 'fs/promises'
 import Path from 'path'
 import { fileURLToPath } from 'url'
-import { importer, selectHasher, selectChunker } from '@organicdesign/db-fs-importer'
+import { importer } from '@organicdesign/db-fs-importer'
+import * as testData from '@organicdesign/db-test-data'
 import { MemoryBlockstore } from 'blockstore-core'
 import all from 'it-all'
 import { exporter } from '../src/index.js'
 
-const inPath = Path.join(Path.dirname(fileURLToPath(import.meta.url)), '../../test-data')
 const outPath = Path.join(Path.dirname(fileURLToPath(import.meta.url)), '../../test-data-out')
 
 const blockstore = new MemoryBlockstore()
-
-const importerConfig = {
-  chunker: selectChunker(),
-  hasher: selectHasher(),
-  cidVersion: 1
-} as const
-
-const paths = [
-  'file-1.txt',
-  'file-2.txt',
-  'dir-1/file-3.txt'
-].map(path => ({
-  in: Path.join(inPath, path),
-  out: Path.join(outPath, path)
-}))
-
-const hash = async (path: string): Promise<Uint8Array> => {
-  const hasher = createHash('sha256')
-
-  hasher.write(await fs.readFile(path))
-
-  return hasher.digest()
-}
 
 describe('exporter', () => {
   beforeEach(async () => {
@@ -46,38 +22,47 @@ describe('exporter', () => {
   })
 
   it('exports files ', async () => {
-    for (const path of paths) {
-      const results = await all(importer(
+    for (const data of testData.data) {
+      const [{ cid }] = await all(importer(
         blockstore,
-        path.in,
-        importerConfig
+        data.path,
+        {}
       ))
 
-      await exporter(blockstore, path.out, results[0].cid)
+      const exportPath = data.generatePath(outPath)
 
-      const hashes = await Promise.all([hash(path.in), hash(path.out)])
+      await exporter(blockstore, exportPath, cid)
 
-      assert.deepEqual(hashes[0], hashes[1])
+      const valid = await data.validate(exportPath)
+
+      assert(valid)
     }
   })
 
   it('exports a directory ', async () => {
     const results = await all(importer(
       blockstore,
-      inPath,
-      importerConfig
+      testData.root,
+      {}
     ))
 
+    assert.equal(results.length, testData.data.length)
+
     for (const { cid, path } of results) {
-      const exportPath = path.replace(inPath, outPath)
+      const dataFile = testData.getDataFile(path)
+
+      assert(dataFile != null)
+
+      const exportPath = dataFile.generatePath(outPath)
 
       await exporter(blockstore, exportPath, cid)
     }
 
-    for (const path of paths) {
-      const hashes = await Promise.all([hash(path.in), hash(path.out)])
+    for (const data of testData.data) {
+      const exportPath = data.generatePath(outPath)
+      const valid = data.validate(exportPath)
 
-      assert.deepEqual(hashes[0], hashes[1])
+      assert(valid)
     }
   })
 })
