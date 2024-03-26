dcd6ed81afe1241462d20ec2af036f5c9ec05acd
saul <saul@organicdesign.co.nz>
2024-03-26 13:34:23 +1300

    Add other methods to paths.

--- packages/test-data/src/generate-paths.ts
+++ packages/test-data/src/generate-paths.ts
@@ -1,21 +1,47 @@
+import { createHash } from 'crypto'
 import fs from 'fs/promises'
 import Path from 'path'
 import { fileURLToPath } from 'url'
-import type { PathInfo } from './interface.js'
+import { compare as uint8ArrayCompareString } from 'uint8arrays/compare'
+import type { TestData } from './interface.js'
 
-export default async (): Promise<PathInfo[]> => {
+const generateHash = async (path: string): Promise<Uint8Array> => {
+  const hasher = createHash('sha256')
+
+  hasher.write(await fs.readFile(path))
+
+  return hasher.digest()
+}
+
+export default async (): Promise<TestData[]> => {
   const rootPath = Path.join(Path.dirname(fileURLToPath(import.meta.url)), '../../test-data')
   const structure = await fs.readdir(rootPath, { recursive: true, withFileTypes: true })
-  const paths: PathInfo[] = []
+  const paths: TestData[] = []
 
   for (const dirent of structure) {
     if (dirent.isDirectory()) {
       continue
     }
 
+    const inPath = Path.join(dirent.path, dirent.name)
+    const inHash = await generateHash(inPath)
+
     paths.push({
-      path: Path.join(dirent.path, dirent.name),
-      name: dirent.name
+      path: inPath,
+      hash: inHash,
+      name: dirent.name,
+
+      generatePath (path: string = '/') {
+        const tail = inPath.replace(rootPath, '')
+
+        return Path.join(path, tail)
+      },
+
+      async validate (path: string): Promise<boolean> {
+        const hash = await generateHash(path)
+
+        return uint8ArrayCompareString(inHash, hash) === 0
+      }
     })
   }
 
--- packages/test-data/src/interface.ts
+++ packages/test-data/src/interface.ts
@@ -1,4 +1,7 @@
-export interface PathInfo {
+export interface TestData {
   name: string
   path: string
+  hash: Uint8Array
+  generatePath(path: string): string
+  validate(path: string): Promise<boolean>
 }
