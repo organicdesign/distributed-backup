b60fc4ddc1f909760120ea2fa38f6d40413a05da
saul <saul@organicdesign.co.nz>
2024-03-28 13:54:45 +1300

    Fix group creation util.

--- packages/daemon/test/utils/create-group.ts
+++ packages/daemon/test/utils/create-group.ts
@@ -1,17 +1,18 @@
 import { type CID } from 'multiformats/cid'
-import type { Provides as GroupsProvides } from '../../src/modules/groups/index.js'
+import type { Groups } from '../../src/common/groups.js'
+import type { Welo } from 'welo'
 
-export const createGroup = async (m: GroupsProvides, name: string, peers: Uint8Array[] = []): Promise<CID> => {
-  const manifest = await m.welo.determine({
+export const createGroup = async (groups: Groups, welo: Welo, name: string, peers: Uint8Array[] = []): Promise<CID> => {
+  const manifest = await welo.determine({
     name,
     meta: { type: 'group' },
     access: {
       protocol: '/hldb/access/static',
-      config: { write: [m.welo.identity.id, ...peers] }
+      config: { write: [welo.identity.id, ...peers] }
     }
   })
 
-  await m.groups.add(manifest)
+  await groups.add(manifest)
 
   return manifest.address.cid
 }
