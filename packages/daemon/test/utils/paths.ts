bb0d1ef96257f6603d77dd130a1f7c1326d06ce6
saul <saul@organicdesign.co.nz>
2024-04-12 13:18:56 +1200

    Fix test path in daemon.

--- packages/daemon/test/utils/paths.ts
+++ packages/daemon/test/utils/paths.ts
@@ -1,6 +1,6 @@
 import Path from 'path'
 import { fileURLToPath } from 'url'
 
-export const testPath = Path.join(Path.dirname(fileURLToPath(import.meta.url)), '../../test-out')
+export const testPath = Path.join(Path.dirname(fileURLToPath(import.meta.url)), '../../../test-out')
 
 export const mkTestPath = (name: string): string => Path.join(testPath, name)
